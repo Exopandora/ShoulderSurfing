e1674c9f7e52486b5e0818f35585705aea6cdfb3
Marcel Konrad <mkonrad98@alice-dsl.net>
2019-10-19 21:13:34 +0200

    Added support for key modifiers

--- src/main/java/com/teamderpy/shouldersurfing/event/KeyHandler.java
+++ src/main/java/com/teamderpy/shouldersurfing/event/KeyHandler.java
@@ -4,9 +4,11 @@ import com.teamderpy.shouldersurfing.ShoulderSurfing;
 import com.teamderpy.shouldersurfing.config.Config;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.InputEvent.KeyInputEvent;
+import net.minecraftforge.client.settings.KeyModifier;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 @OnlyIn(Dist.CLIENT)
@@ -19,31 +21,41 @@ public class KeyHandler
 		{
 			if(Minecraft.getInstance().gameSettings.thirdPersonView == Config.CLIENT.getShoulderSurfing3ppId())
 			{
-				if(ShoulderSurfing.KEYBIND_ROTATE_CAMERA_LEFT.isKeyDown())
+				if(KeyHandler.isKeyDown(ShoulderSurfing.KEYBIND_ROTATE_CAMERA_LEFT))
 				{
 					Config.CLIENT.adjustCameraLeft();
 				}
 				
-				if(ShoulderSurfing.KEYBIND_ROTATE_CAMERA_RIGHT.isKeyDown())
+				if(KeyHandler.isKeyDown(ShoulderSurfing.KEYBIND_ROTATE_CAMERA_RIGHT))
 				{
 					Config.CLIENT.adjustCameraRight();
 				}
 				
-				if(ShoulderSurfing.KEYBIND_ZOOM_CAMERA_IN.isKeyDown())
+				if(KeyHandler.isKeyDown(ShoulderSurfing.KEYBIND_ZOOM_CAMERA_IN))
 				{
 					Config.CLIENT.adjustCameraIn();
 				}
 				
-				if(ShoulderSurfing.KEYBIND_ZOOM_CAMERA_OUT.isKeyDown())
+				if(KeyHandler.isKeyDown(ShoulderSurfing.KEYBIND_ZOOM_CAMERA_OUT))
 				{
 					Config.CLIENT.adjustCameraOut();
 				}
 				
-				if(ShoulderSurfing.KEYBIND_SWAP_SHOULDER.isPressed())
+				if(KeyHandler.isPressed(ShoulderSurfing.KEYBIND_SWAP_SHOULDER))
 				{
 					Config.CLIENT.swapShoulder();
 				}
 			}
 		}
+	}
+	
+	private static boolean isPressed(KeyBinding keyBinding)
+	{
+		return keyBinding.isPressed() && (KeyModifier.NONE.equals(keyBinding.getKeyModifier()) || KeyModifier.getActiveModifier().equals(keyBinding.getKeyModifier()));
+	}
+	
+	private static boolean isKeyDown(KeyBinding keyBinding)
+	{
+		return keyBinding.isKeyDown() && (KeyModifier.NONE.equals(keyBinding.getKeyModifier()) || KeyModifier.getActiveModifier().equals(keyBinding.getKeyModifier()));
 	}
 }
