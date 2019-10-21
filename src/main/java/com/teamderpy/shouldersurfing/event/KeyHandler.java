8f76d44d267a98d6c65f1fef281c8dbf92abef66
Marcel Konrad <mkonrad98@alice-dsl.net>
2019-10-21 19:30:49 +0200

    Implement toggle key for shoulder surfing perspective

--- src/main/java/com/teamderpy/shouldersurfing/event/KeyHandler.java
+++ src/main/java/com/teamderpy/shouldersurfing/event/KeyHandler.java
@@ -3,6 +3,7 @@ package com.teamderpy.shouldersurfing.event;
 import org.lwjgl.glfw.GLFW;
 
 import com.teamderpy.shouldersurfing.config.Config;
+import com.teamderpy.shouldersurfing.config.Config.ClientConfig.Perspective;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
@@ -15,18 +16,33 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 @OnlyIn(Dist.CLIENT)
 public class KeyHandler
 {
-	public static final KeyBinding KEYBIND_ROTATE_CAMERA_LEFT = new KeyBinding("Camera left", GLFW.GLFW_KEY_LEFT, "Shoulder Surfing");
-	public static final KeyBinding KEYBIND_ROTATE_CAMERA_RIGHT = new KeyBinding("Camera right", GLFW.GLFW_KEY_RIGHT, "Shoulder Surfing");
-	public static final KeyBinding KEYBIND_ZOOM_CAMERA_OUT = new KeyBinding("Camera closer", GLFW.GLFW_KEY_UP, "Shoulder Surfing");
-	public static final KeyBinding KEYBIND_ZOOM_CAMERA_IN = new KeyBinding("Camera farther", GLFW.GLFW_KEY_DOWN, "Shoulder Surfing");
-	public static final KeyBinding KEYBIND_SWAP_SHOULDER = new KeyBinding("Swap shoulder", GLFW.GLFW_KEY_O, "Shoulder Surfing");
+	private static final String KEY_CATEGORY = "Shoulder Surfing";
+	
+	public static final KeyBinding KEYBIND_ROTATE_CAMERA_LEFT = new KeyBinding("Camera left", GLFW.GLFW_KEY_LEFT, KeyHandler.KEY_CATEGORY);
+	public static final KeyBinding KEYBIND_ROTATE_CAMERA_RIGHT = new KeyBinding("Camera right", GLFW.GLFW_KEY_RIGHT, KeyHandler.KEY_CATEGORY);
+	public static final KeyBinding KEYBIND_ZOOM_CAMERA_OUT = new KeyBinding("Camera closer", GLFW.GLFW_KEY_UP, KeyHandler.KEY_CATEGORY);
+	public static final KeyBinding KEYBIND_ZOOM_CAMERA_IN = new KeyBinding("Camera farther", GLFW.GLFW_KEY_DOWN, KeyHandler.KEY_CATEGORY);
+	public static final KeyBinding KEYBIND_SWAP_SHOULDER = new KeyBinding("Swap shoulder", GLFW.GLFW_KEY_O, KeyHandler.KEY_CATEGORY);
+	public static final KeyBinding KEYBIND_TOGGLE_SHOULDER_SURFING = new KeyBinding("Toggle perspective", GLFW.GLFW_KEY_F6, KeyHandler.KEY_CATEGORY);
 	
 	@SubscribeEvent
 	public static void keyInputEvent(KeyInputEvent event)
 	{
 		if(Minecraft.getInstance() != null && Minecraft.getInstance().currentScreen == null)
 		{
-			if(Minecraft.getInstance().gameSettings.thirdPersonView == Config.CLIENT.getShoulderSurfing3ppId())
+			if(KeyHandler.isPressed(KeyHandler.KEYBIND_TOGGLE_SHOULDER_SURFING))
+			{
+				if(Minecraft.getInstance().gameSettings.thirdPersonView == Perspective.SHOULDER_SURFING.getPerspectiveId())
+				{
+					Minecraft.getInstance().gameSettings.thirdPersonView = Perspective.FIRST_PERSON.getPerspectiveId();
+				}
+				else if(Minecraft.getInstance().gameSettings.thirdPersonView == Perspective.FIRST_PERSON.getPerspectiveId())
+				{
+					Minecraft.getInstance().gameSettings.thirdPersonView = Perspective.SHOULDER_SURFING.getPerspectiveId();
+				}
+			}
+			
+			if(Minecraft.getInstance().gameSettings.thirdPersonView == Perspective.SHOULDER_SURFING.getPerspectiveId())
 			{
 				if(KeyHandler.isKeyDown(KeyHandler.KEYBIND_ROTATE_CAMERA_LEFT))
 				{
