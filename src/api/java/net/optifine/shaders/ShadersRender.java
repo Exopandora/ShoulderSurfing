dc2db866ef7dad033da6813fdd5f98cf41c53e7a
Marcel Konrad <mkonrad98@alice-dsl.net>
2021-01-17 00:19:15 +0100

    Update OptiFine API

---
+++ src/api/java/net/optifine/shaders/ShadersRender.java
@@ -0,0 +1,12 @@
+package net.optifine.shaders;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.ActiveRenderInfo;
+
+public class ShadersRender
+{
+	public static void updateActiveRenderInfo(ActiveRenderInfo activeRenderInfo, Minecraft mc, float partialTicks)
+	{
+		return;
+	}
+}
