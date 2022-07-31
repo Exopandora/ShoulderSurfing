8b7a855523d729bbf631074beb80526a9cee6bdc
Marcel Konrad <mkonrad98@alice-dsl.net>
2022-07-31 22:21:07 +0200

    Fixes #80

--- fabric/src/main/java/com/teamderpy/shouldersurfing/mixins/MixinGui2.java
+++ fabric/src/main/java/com/teamderpy/shouldersurfing/mixins/MixinGui2.java
@@ -3,6 +3,7 @@ package com.teamderpy.shouldersurfing.mixins;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
@@ -20,20 +21,30 @@ public class MixinGui2
 	
 	@Inject
 	(
-		method = "renderCrosshair",
-		at = @At("HEAD")
+		method = "render",
+		at = @At
+		(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/gui/Gui;renderCrosshair(Lcom/mojang/blaze3d/vertex/PoseStack;)V",
+			shift = Shift.BEFORE
+		)
 	)
-	private void offsetCrosshair(PoseStack poseStack, CallbackInfo ci)
+	private void offsetCrosshair(PoseStack poseStack, float partialTick, CallbackInfo ci)
 	{
 		ShoulderRenderer.getInstance().offsetCrosshair(poseStack, this.minecraft.getWindow(), this.minecraft.getFrameTime());
 	}
 	
 	@Inject
 	(
-		method = "renderCrosshair",
-		at = @At("TAIL")
+		method = "render",
+		at = @At
+		(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/gui/Gui;renderCrosshair(Lcom/mojang/blaze3d/vertex/PoseStack;)V",
+			shift = Shift.AFTER
+		)
 	)
-	private void clearCrosshairOffset(PoseStack poseStack, CallbackInfo ci)
+	private void clearCrosshairOffset(PoseStack poseStack, float partialTick, CallbackInfo ci)
 	{
 		ShoulderRenderer.getInstance().clearCrosshairOffset(poseStack);
 	}
