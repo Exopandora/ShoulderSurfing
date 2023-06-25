da939ac66541435de854ec4fe2725d5d56bc578a
Exopandora <mkonrad98@alice-dsl.net>
2023-06-25 23:12:44 +0200

    Add dynamic with 1pp crosshair type. Closes #121

--- common/src/main/java/com/teamderpy/shouldersurfing/config/CrosshairType.java
+++ common/src/main/java/com/teamderpy/shouldersurfing/config/CrosshairType.java
@@ -7,7 +7,8 @@ public enum CrosshairType
 	ADAPTIVE,
 	DYNAMIC,
 	STATIC,
-	STATIC_WITH_1PP;
+	STATIC_WITH_1PP,
+	DYNAMIC_WITH_1PP;
 	
 	public boolean isDynamic()
 	{
@@ -15,7 +16,7 @@ public enum CrosshairType
 		{
 			return ShoulderHelper.isHoldingAdaptiveItem();
 		}
-		else if(this == CrosshairType.DYNAMIC)
+		else if(this == CrosshairType.DYNAMIC || this == CrosshairType.DYNAMIC_WITH_1PP)
 		{
 			return true;
 		}
@@ -25,7 +26,7 @@ public enum CrosshairType
 	
 	public boolean doSwitchPerspective()
 	{
-		if(this == CrosshairType.STATIC_WITH_1PP)
+		if(this == CrosshairType.STATIC_WITH_1PP || this == CrosshairType.DYNAMIC_WITH_1PP)
 		{
 			return ShoulderHelper.isHoldingAdaptiveItem();
 		}
