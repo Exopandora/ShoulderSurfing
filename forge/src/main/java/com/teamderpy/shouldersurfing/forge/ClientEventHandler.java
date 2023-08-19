f518ec5661198e3d5a1abc31bd9d923170550b5d
Exopandora <mkonrad98@outlook.com>
2023-08-19 19:29:03 +0200

    Dynamically adjust x and y offset depending on space constraints

--- forge/src/main/java/com/teamderpy/shouldersurfing/forge/ClientEventHandler.java
+++ forge/src/main/java/com/teamderpy/shouldersurfing/forge/ClientEventHandler.java
@@ -55,7 +55,7 @@ public class ClientEventHandler
 	@SuppressWarnings("resource")
 	public static void computeCameraAnglesEvent(CameraSetup event)
 	{
-		ShoulderRenderer.getInstance().offsetCamera(event.getCamera(), Minecraft.getInstance().level, event.getPartialTicks());
+		ShoulderRenderer.getInstance().offsetCamera(event.getCamera(), Minecraft.getInstance().level, (float) event.getPartialTick());
 	}
 	
 	@SubscribeEvent
