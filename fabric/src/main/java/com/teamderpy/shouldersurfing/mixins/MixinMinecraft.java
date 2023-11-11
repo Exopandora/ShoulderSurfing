b05856df0468b12d810399302f6b72047d91f7dd
Exopandora <mkonrad98@outlook.com>
2023-11-11 17:16:33 +0100

    Partially revert "Rework perspective change override. Fixes #135"
    This reverts commit dc8d4417d647fafeb03dc7d0dd3265d2722da2f2.

--- fabric/src/main/java/com/teamderpy/shouldersurfing/mixins/MixinMinecraft.java
+++ fabric/src/main/java/com/teamderpy/shouldersurfing/mixins/MixinMinecraft.java
@@ -13,7 +13,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.fml.event.config.ModConfigEvent;
 
 @Mixin(Minecraft.class)
-public class MixinMinecraft2
+public class MixinMinecraft
 {
 	@Inject
 	(
