4f390575f23da28c8efec2f7551888d5db7a9579
Exopandora <mkonrad98@outlook.com>
2024-02-07 19:46:57 +0100

    Update parameter names of MixinRenderType

--- common/src/main/java/com/teamderpy/shouldersurfing/mixins/MixinRenderType.java
+++ common/src/main/java/com/teamderpy/shouldersurfing/mixins/MixinRenderType.java
@@ -10,9 +10,9 @@ import net.minecraft.client.renderer.RenderType;
 @Mixin(RenderType.class)
 public abstract class MixinRenderType extends RenderStateShard
 {
-	public MixinRenderType(String $$0, Runnable $$1, Runnable $$2)
+	public MixinRenderType(String name, Runnable setupState, Runnable clearState)
 	{
-		super($$0, $$1, $$2);
+		super(name, setupState, clearState);
 	}
 	
 	@ModifyArg
@@ -25,7 +25,7 @@ public abstract class MixinRenderType extends RenderStateShard
 		),
 		require = 1
 	)
-	private static RenderStateShard.TransparencyStateShard setTransparencyState(RenderStateShard.TransparencyStateShard p_110686_)
+	private static RenderStateShard.TransparencyStateShard setTransparencyState(RenderStateShard.TransparencyStateShard transparencyStateShard)
 	{
 		return TRANSLUCENT_TRANSPARENCY;
 	}
