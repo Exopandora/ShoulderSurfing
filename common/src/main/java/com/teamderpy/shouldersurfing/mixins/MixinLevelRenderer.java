01d3b0d14d255be921af4898e58f500480323d96
Exopandora <mkonrad98@outlook.com>
2024-02-17 19:23:07 +0100

    Use more robust method to manipulate player transparency and visibility

---
+++ common/src/main/java/com/teamderpy/shouldersurfing/mixins/MixinLevelRenderer.java
@@ -0,0 +1,45 @@
+package com.teamderpy.shouldersurfing.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.teamderpy.shouldersurfing.client.ShoulderRenderer;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.world.entity.Entity;
+
+@Mixin(LevelRenderer.class)
+public class MixinLevelRenderer
+{
+	@Inject
+	(
+		method = "renderEntity",
+		at = @At("HEAD"),
+		cancellable = true
+	)
+	public void preRender(Entity entity, double x, double y, double z, float partialTick, PoseStack poseStack, MultiBufferSource multiBufferSource, CallbackInfo ci)
+	{
+		if(entity == Minecraft.getInstance().getCameraEntity() && ShoulderRenderer.getInstance().preRenderCameraEntity(entity, partialTick))
+		{
+			ci.cancel();
+		}
+	}
+	
+	@Inject
+	(
+		method = "renderEntity",
+		at = @At("TAIL")
+	)
+	public void postRender(Entity entity, double x, double y, double z, float partialTick, PoseStack poseStack, MultiBufferSource multiBufferSource, CallbackInfo ci)
+	{
+		if(entity == Minecraft.getInstance().getCameraEntity())
+		{
+			ShoulderRenderer.getInstance().postRenderCameraEntity(entity, partialTick, multiBufferSource);
+		}
+	}
+}
