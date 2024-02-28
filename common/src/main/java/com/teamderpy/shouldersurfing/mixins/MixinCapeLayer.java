2c9efaecb93b0be0615c6131733084005b6a2631
Exopandora <mkonrad98@outlook.com>
2024-02-28 18:20:20 +0100

    Fix cape transparency

---
+++ common/src/main/java/com/teamderpy/shouldersurfing/mixins/MixinCapeLayer.java
@@ -0,0 +1,27 @@
+package com.teamderpy.shouldersurfing.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.layers.CapeLayer;
+import net.minecraft.util.ResourceLocation;
+
+@Mixin(CapeLayer.class)
+public class MixinCapeLayer
+{
+	@Redirect
+	(
+		method = "render",
+		at = @At
+		(
+			value = "INVOKE",
+			target = "net/minecraft/client/renderer/RenderType.entitySolid(Lnet/minecraft/util/ResourceLocation;)Lnet/minecraft/client/renderer/RenderType;"
+		)
+	)
+	private RenderType entitySolid(ResourceLocation texture)
+	{
+		return RenderType.itemEntityTranslucentCull(texture);
+	}
+}
