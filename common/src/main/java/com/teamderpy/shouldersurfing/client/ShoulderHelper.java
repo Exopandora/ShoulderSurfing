ebb4c633e1317a9740a5415235a924acc3d2951f
Exopandora <mkonrad98@outlook.com>
2024-03-13 23:34:19 +0100

    Fix player reach

--- common/src/main/java/com/teamderpy/shouldersurfing/client/ShoulderHelper.java
+++ common/src/main/java/com/teamderpy/shouldersurfing/client/ShoulderHelper.java
@@ -94,7 +94,7 @@ public class ShoulderHelper
 			playerReach = Math.max(playerReach, gameMode.getPlayerMode().isCreative() ? 6.0D : 3.0D);
 		}
 		
-		if(blockHit != null)
+		if(blockHit.getType() != HitResult.Type.MISS)
 		{
 			playerReach = blockHit.getLocation().distanceTo(eyePosition);
 		}
@@ -104,8 +104,8 @@ public class ShoulderHelper
 		if(entityHit != null)
 		{
 			double distance = eyePosition.distanceTo(entityHit.getLocation());
-
-			if(distance < playerReach || blockHit == null)
+			
+			if(distance < playerReach || blockHit.getType() != HitResult.Type.MISS)
 			{
 				return entityHit;
 			}
