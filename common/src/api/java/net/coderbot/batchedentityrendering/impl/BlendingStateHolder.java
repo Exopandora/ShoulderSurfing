f00a7bfbe42d20ccd5757a85422a9bc04c907f88
Exopandora <mkonrad98@outlook.com>
2024-03-13 21:20:13 +0100

    Fix compatibility with sodium and oculus. Closes #160

---
+++ common/src/api/java/net/coderbot/batchedentityrendering/impl/BlendingStateHolder.java
@@ -0,0 +1,8 @@
+package net.coderbot.batchedentityrendering.impl;
+
+public interface BlendingStateHolder
+{
+	TransparencyType getTransparencyType();
+	
+	void setTransparencyType(TransparencyType transparencyType);
+}
