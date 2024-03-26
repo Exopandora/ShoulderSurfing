4d406367013500b454eb4585986d6575b6d7e6d8
Exopandora <mkonrad98@outlook.com>
2024-03-26 18:58:11 +0100

    Reset camera rotations when config reloads and camera is not decoupled

--- common/src/main/java/com/github/exopandora/shouldersurfing/config/Config.java
+++ common/src/main/java/com/github/exopandora/shouldersurfing/config/Config.java
@@ -6,6 +6,9 @@ import java.util.List;
 import java.util.Map;
 
 import com.github.exopandora.shouldersurfing.client.ShoulderInstance;
+import com.github.exopandora.shouldersurfing.client.ShoulderRenderer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.Entity;
 import org.apache.commons.lang3.tuple.Pair;
 
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -792,6 +795,14 @@ public class Config
 		if(ShoulderInstance.getInstance() != null && Type.CLIENT.equals(config.getType()) && Config.CLIENT.doRememberLastPerspective())
 		{
 			Config.CLIENT.setDefaultPerspective(Perspective.current());
+		}
+		
+		Entity cameraEntity = Minecraft.getInstance().getCameraEntity();
+		
+		if(cameraEntity != null && !Config.CLIENT.isCameraDecoupled())
+		{
+			ShoulderInstance.getInstance().resetCameraEntityRotations(cameraEntity);
+			ShoulderRenderer.getInstance().resetCameraRotations(cameraEntity);
 		}
 	}
 }
