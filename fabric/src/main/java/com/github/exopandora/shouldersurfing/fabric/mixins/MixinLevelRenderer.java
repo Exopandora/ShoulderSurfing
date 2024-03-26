42d0f3d17ec2d1896f371e8bdd7a7241def334b7
Exopandora <mkonrad98@outlook.com>
2024-03-26 19:00:20 +0100

    Remove suffix from platform specific mixins

--- fabric/src/main/java/com/github/exopandora/shouldersurfing/fabric/mixins/MixinLevelRenderer.java
+++ fabric/src/main/java/com/github/exopandora/shouldersurfing/fabric/mixins/MixinLevelRenderer.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LevelRenderer.class)
-public class MixinLevelRendererFabric
+public class MixinLevelRenderer
 {
 	@Inject
 	(
