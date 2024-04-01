be5859540d4d953f256bb284da7a3107e51ed9b9
Exopandora <mkonrad98@outlook.com>
2024-04-01 15:30:54 +0200

    Document incompatible mods

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -129,6 +129,7 @@ publishMods {
 		clientRequired = true
 		serverRequired = false
 		requires("fabric-api", "forge-config-api-port-fabric")
+		incompatible("better-third-person", "cameraoverhaul", "nimble-fabric")
 	}
 	
 	modrinth {
@@ -136,5 +137,6 @@ publishMods {
 		accessToken = findProperty("modrinth_api_key").toString()
 		minecraftVersions.set(compatibleVersions)
 		requires("fabric-api", "forge-config-api-port")
+		incompatible("better-third-person", "cameraoverhaul", "nimble")
 	}
 }
