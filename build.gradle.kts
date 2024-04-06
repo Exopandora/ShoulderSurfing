567ffe107efeb61a0702f00b172519d636eaf55a
Exopandora <mkonrad98@outlook.com>
2024-04-06 17:57:28 +0200

    Refactor mod metadata

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.configurationcache.extensions.capitalized
+
 plugins {
 	id("java")
 }
@@ -52,8 +54,8 @@ subprojects {
 				"Specification-Title" to modName,
 				"Specification-Vendor" to modAuthor,
 				"Specification-Version" to modVersion,
-				"Implementation-Title" to project.name,
-				"Implementation-Version" to modVersion,
+				"Implementation-Title" to project.name.capitalized(),
+				"Implementation-Version" to "${libs.versions.minecraft.get()}-$modVersion",
 				"Implementation-Vendor" to modAuthor
 			))
 		}
