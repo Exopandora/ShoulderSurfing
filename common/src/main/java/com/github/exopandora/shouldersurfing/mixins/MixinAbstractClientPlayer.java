671ba1d7fae1595481d3ee6f5aaddb4d146e7223
Exopandora <mkonrad98@outlook.com>
2024-04-06 19:33:49 +0200

    Interpolate player movement when fall flying. Fixes MC-194080

--- common/src/main/java/com/github/exopandora/shouldersurfing/mixins/MixinAbstractClientPlayer.java
+++ common/src/main/java/com/github/exopandora/shouldersurfing/mixins/MixinAbstractClientPlayer.java
@@ -1,16 +1,43 @@
 package com.github.exopandora.shouldersurfing.mixins;
 
 import com.github.exopandora.shouldersurfing.config.Perspective;
+import com.github.exopandora.shouldersurfing.mixinducks.AbstractClientPlayerDuck;
+import com.mojang.authlib.GameProfile;
+import net.minecraft.client.CameraType;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import net.minecraft.client.CameraType;
-import net.minecraft.client.player.AbstractClientPlayer;
-
 @Mixin(AbstractClientPlayer.class)
-public class MixinAbstractClientPlayer
+public abstract class MixinAbstractClientPlayer extends Player implements AbstractClientPlayerDuck
 {
+	@Unique
+	private Vec3 deltaMovementOnPreviousTick = Vec3.ZERO;
+	
+	public MixinAbstractClientPlayer(Level level, BlockPos pos, float yRot, GameProfile gameProfile)
+	{
+		super(level, pos, yRot, gameProfile);
+	}
+	
+	@Override
+	public void tick()
+	{
+		this.deltaMovementOnPreviousTick = this.getDeltaMovement();
+		super.tick();
+	}
+	
+	@Override
+	public Vec3 shouldersurfing$getDeltaMovementLerped(float partialTick)
+	{
+		return this.deltaMovementOnPreviousTick.lerp(this.getDeltaMovement(), partialTick);
+	}
+	
 	@Redirect
 	(
 		method = "getFieldOfViewModifier",
