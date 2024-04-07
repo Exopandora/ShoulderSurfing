fac0afef71c91ad123a7a86a8e3823a3a56aa560
Exopandora <mkonrad98@outlook.com>
2024-04-07 17:20:24 +0200

    Refactor player turning modification

---
+++ common/src/main/java/com/github/exopandora/shouldersurfing/mixins/MixinLocalPlayer.java
@@ -0,0 +1,28 @@
+package com.github.exopandora.shouldersurfing.mixins;
+
+import com.github.exopandora.shouldersurfing.client.ShoulderRenderer;
+import com.mojang.authlib.GameProfile;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.entity.player.ProfilePublicKey;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(LocalPlayer.class)
+public abstract class MixinLocalPlayer extends AbstractClientPlayer
+{
+	public MixinLocalPlayer(ClientLevel level, GameProfile gameProfile, @Nullable ProfilePublicKey profilePublicKey)
+	{
+		super(level, gameProfile, profilePublicKey);
+	}
+	
+	@Override
+	public void turn(double yRot, double xRot)
+	{
+		if(!ShoulderRenderer.getInstance().turn(this, yRot, xRot))
+		{
+			super.turn(yRot, xRot);
+		}
+	}
+}
--- common/src/main/java/com/github/exopandora/shouldersurfing/mixins/MixinMouseHandler.java
+++ common/src/main/java/com/github/exopandora/shouldersurfing/mixins/MixinMouseHandler.java
@@ -1,10 +1,8 @@
 package com.github.exopandora.shouldersurfing.mixins;
 
-import com.github.exopandora.shouldersurfing.client.ShoulderRenderer;
 import com.github.exopandora.shouldersurfing.config.Perspective;
 import net.minecraft.client.CameraType;
 import net.minecraft.client.MouseHandler;
-import net.minecraft.client.player.LocalPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -24,22 +22,5 @@ public class MixinMouseHandler
 	private boolean isFirstPerson(CameraType cameraType)
 	{
 		return cameraType.isFirstPerson() || Perspective.SHOULDER_SURFING.equals(Perspective.current());
-	}
-	
-	@Redirect
-	(
-		method = "turnPlayer",
-		at = @At
-		(
-			value = "INVOKE",
-			target = "net/minecraft/client/player/LocalPlayer.turn(DD)V"
-		)
-	)
-	private void turn(LocalPlayer player, double yRot, double xRot)
-	{
-		if(!ShoulderRenderer.getInstance().turn(player, yRot, xRot))
-		{
-			player.turn(yRot, xRot);
-		}
 	}
 }
