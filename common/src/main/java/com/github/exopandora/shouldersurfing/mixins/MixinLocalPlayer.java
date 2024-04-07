b8d30205c430f7730bd64a09e63330b4e57d354c
Exopandora <mkonrad98@outlook.com>
2024-04-07 17:20:24 +0200

    Refactor player turning modification

---
+++ common/src/main/java/com/github/exopandora/shouldersurfing/mixins/MixinLocalPlayer.java
@@ -0,0 +1,26 @@
+package com.github.exopandora.shouldersurfing.mixins;
+
+import com.github.exopandora.shouldersurfing.client.ShoulderRenderer;
+import com.mojang.authlib.GameProfile;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.player.LocalPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(LocalPlayer.class)
+public abstract class MixinLocalPlayer extends AbstractClientPlayer
+{
+	public MixinLocalPlayer(ClientLevel level, GameProfile gameProfile)
+	{
+		super(level, gameProfile);
+	}
+	
+	@Override
+	public void turn(double yRot, double xRot)
+	{
+		if(!ShoulderRenderer.getInstance().turn(this, yRot, xRot))
+		{
+			super.turn(yRot, xRot);
+		}
+	}
+}
