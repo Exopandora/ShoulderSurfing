d8a11642a12af8873201eb09d6a1d7de5480a887
Exopandora <mkonrad98@outlook.com>
2024-04-07 15:24:39 +0200

    Refactor keybinds

--- fabric/src/main/java/com/github/exopandora/shouldersurfing/fabric/mixins/MixinMinecraft.java
+++ fabric/src/main/java/com/github/exopandora/shouldersurfing/fabric/mixins/MixinMinecraft.java
@@ -1,16 +1,16 @@
 package com.github.exopandora.shouldersurfing.fabric.mixins;
 
-import com.github.exopandora.shouldersurfing.client.ShoulderRenderer;
+import com.github.exopandora.shouldersurfing.client.KeyHandler;
 import com.github.exopandora.shouldersurfing.client.ShoulderInstance;
+import com.github.exopandora.shouldersurfing.client.ShoulderRenderer;
 import com.github.exopandora.shouldersurfing.config.Config;
+import net.minecraft.client.GameConfiguration;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.fml.event.config.ModConfigEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import net.minecraft.client.GameConfiguration;
-import net.minecraft.client.Minecraft;
-import net.minecraftforge.api.fml.event.config.ModConfigEvent;
 
 @Mixin(Minecraft.class)
 public class MixinMinecraft
@@ -38,5 +38,15 @@ public class MixinMinecraft
 			ShoulderInstance.getInstance().tick();
 			ShoulderRenderer.getInstance().tick();
 		}
+	}
+	
+	@Inject
+	(
+		at = @At("HEAD"),
+		method = "handleKeybinds"
+	)
+	private void handleKeybinds(CallbackInfo info)
+	{
+		KeyHandler.tick();
 	}
 }
--- forge/src/main/java/com/github/exopandora/shouldersurfing/forge/event/ClientEventHandler.java
+++ forge/src/main/java/com/github/exopandora/shouldersurfing/forge/event/ClientEventHandler.java
@@ -58,7 +58,10 @@ public class ClientEventHandler
 	@SubscribeEvent
 	public static void keyInputEvent(InputEvent event)
 	{
-		KeyHandler.tick();
+		if(Minecraft.getInstance().screen == null)
+		{
+			KeyHandler.tick();
+		}
 	}
 	
 	@SubscribeEvent
