44882e29adb1e0e69c3feeed3290d6ac587a03fe
Exopandora <mkonrad98@outlook.com>
2024-05-06 18:16:56 +0200

    Reset camera rotations when another mod changes to shoulder surfing perspective. Closes #182

---
+++ common/src/main/java/com/github/exopandora/shouldersurfing/mixinducks/OptionsDuck.java
@@ -0,0 +1,8 @@
+package com.github.exopandora.shouldersurfing.mixinducks;
+
+import net.minecraft.client.CameraType;
+
+public interface OptionsDuck
+{
+	void setCameraTypeDirect(CameraType cameraType);
+}
