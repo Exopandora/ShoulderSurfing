7f5ec09480addda5deda9f4c3272a41a36ceae73
Exopandora <mkonrad98@outlook.com>
2024-05-06 18:56:09 +0200

    Add mixin to make camera entity transparency more robust

---
+++ common/src/main/java/com/github/exopandora/shouldersurfing/mixins/MixinModelPart$Cube.java
@@ -0,0 +1,23 @@
+package com.github.exopandora.shouldersurfing.mixins;
+
+import com.github.exopandora.shouldersurfing.client.ShoulderRenderer;
+import net.minecraft.client.model.geom.ModelPart;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(ModelPart.Cube.class)
+public class MixinModelPart$Cube
+{
+	@ModifyVariable
+	(
+		at = @At("HEAD"),
+		method = "compile",
+		index = 8,
+		argsOnly = true
+	)
+	public float compile(float alpha)
+	{
+		return Math.min(alpha, ShoulderRenderer.getInstance().getCameraEntityAlpha());
+	}
+}
