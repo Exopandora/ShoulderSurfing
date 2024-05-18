bca2503faa7ede7b4ebb69188ffc62af31e8bccb
Exopandora <mkonrad98@outlook.com>
2024-05-18 19:45:54 +0200

    Align final shadow field declaration

--- fabric/src/main/java/com/github/exopandora/shouldersurfing/fabric/mixins/MixinGameRenderer.java
+++ fabric/src/main/java/com/github/exopandora/shouldersurfing/fabric/mixins/MixinGameRenderer.java
@@ -1,5 +1,10 @@
 package com.github.exopandora.shouldersurfing.fabric.mixins;
 
+import com.github.exopandora.shouldersurfing.client.ShoulderRenderer;
+import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GameRenderer;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -7,18 +12,11 @@ import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.github.exopandora.shouldersurfing.client.ShoulderRenderer;
-
-import net.minecraft.client.Camera;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.GameRenderer;
-
 @Mixin(GameRenderer.class)
 public class MixinGameRenderer
 {
 	@Shadow
-	private Camera mainCamera;
+	private @Final Camera mainCamera;
 	
 	@Inject
 	(
