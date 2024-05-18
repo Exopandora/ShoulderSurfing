54f9e4be9291fd8f12a9c15f0e3f05726c3eacb1
Exopandora <mkonrad98@outlook.com>
2024-05-18 19:27:02 +0200

    Use shadow field

--- common/src/main/java/com/github/exopandora/shouldersurfing/mixins/MixinGui.java
+++ common/src/main/java/com/github/exopandora/shouldersurfing/mixins/MixinGui.java
@@ -1,20 +1,24 @@
 package com.github.exopandora.shouldersurfing.mixins;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
 import com.github.exopandora.shouldersurfing.client.ShoulderInstance;
 import com.github.exopandora.shouldersurfing.config.Config;
 import com.github.exopandora.shouldersurfing.config.Perspective;
-
 import net.minecraft.client.CameraType;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Gui.class)
 public class MixinGui
 {
+	@Final
+	@Shadow
+	private Minecraft minecraft;
+	
 	@Redirect
 	(
 		method = "renderCrosshair",
@@ -26,6 +30,6 @@ public class MixinGui
 	)
 	private boolean doRenderCrosshair(CameraType cameraType)
 	{
-		return Config.CLIENT.getCrosshairVisibility(Perspective.current()).doRender(Minecraft.getInstance().hitResult, ShoulderInstance.getInstance().isAiming());
+		return Config.CLIENT.getCrosshairVisibility(Perspective.current()).doRender(this.minecraft.hitResult, ShoulderInstance.getInstance().isAiming());
 	}
 }
