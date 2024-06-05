073afdfd6b4864d0c06c402c61719d965402abc5
Exopandora <mkonrad98@outlook.com>
2024-06-05 22:30:21 +0200

    Support TheOneProbe

---
+++ common/src/main/java/com/github/exopandora/shouldersurfing/mixins/compat/theoneprobe/MixinOverlayRenderer.java
@@ -0,0 +1,47 @@
+package com.github.exopandora.shouldersurfing.mixins.compat.theoneprobe;
+
+import com.github.exopandora.shouldersurfing.api.model.PickContext;
+import com.github.exopandora.shouldersurfing.client.ShoulderSurfingImpl;
+import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Pseudo
+@Mixin(targets = "mcjty.theoneprobe.rendering.OverlayRenderer")
+public class MixinOverlayRenderer
+{
+	@Redirect
+	(
+		method = "renderHUD",
+		at = @At
+		(
+			value = "NEW",
+			target = "Lnet/minecraft/world/level/ClipContext;",
+			remap = true
+		),
+		remap = false
+	)
+	private static ClipContext initClipContext(Vec3 start, Vec3 end, ClipContext.Block blockContext, ClipContext.Fluid fluidContext, @NotNull Entity entity)
+	{
+		if(ShoulderSurfingImpl.getInstance().isShoulderSurfing())
+		{
+			Minecraft minecraft = Minecraft.getInstance();
+			Camera camera = minecraft.gameRenderer.getMainCamera();
+			PickContext pickContext = new PickContext.Builder(camera)
+				.withFluidContext(fluidContext)
+				.withEntity(entity)
+				.build();
+			return pickContext.toClipContext(start.distanceTo(end), minecraft.getFrameTime());
+		}
+		
+		return new ClipContext(start, end, blockContext, fluidContext, entity);
+	}
+}
