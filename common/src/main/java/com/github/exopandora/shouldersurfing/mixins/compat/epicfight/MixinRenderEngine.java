f9cffa4d6c08cb196b23ba57530600199797c6f6
Exopandora <mkonrad98@outlook.com>
2024-06-05 20:22:45 +0200

    Fix epicfight compatibility

---
+++ common/src/main/java/com/github/exopandora/shouldersurfing/mixins/compat/epicfight/MixinRenderEngine.java
@@ -0,0 +1,33 @@
+package com.github.exopandora.shouldersurfing.mixins.compat.epicfight;
+
+import com.github.exopandora.shouldersurfing.client.ShoulderSurfingImpl;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Pseudo
+@Mixin(targets = "yesman.epicfight.client.events.engine.RenderEngine")
+public class MixinRenderEngine
+{
+	@Inject
+	(
+		method = "correctCamera",
+		at = @At
+		(
+			value = "INVOKE",
+			target = "net/minecraft/client/settings/PointOfView.isMirrored()Z",
+			remap = true
+		),
+		cancellable = true,
+		remap = false
+	)
+	private void correctCamera(CallbackInfo ci)
+	{
+		if(ShoulderSurfingImpl.getInstance().isShoulderSurfing())
+		{
+			ci.cancel();
+		}
+	}
+}
