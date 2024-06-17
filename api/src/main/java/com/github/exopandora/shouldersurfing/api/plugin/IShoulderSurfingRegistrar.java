f43d628b98c78c52f858c8ed34a931a8e14d8eb3
Exopandora <mkonrad98@outlook.com>
2024-06-17 21:28:25 +0200

    Add registerTargetCameraOffsetCallback method to IShoulderSurfingRegistrar

--- api/src/main/java/com/github/exopandora/shouldersurfing/api/plugin/IShoulderSurfingRegistrar.java
+++ api/src/main/java/com/github/exopandora/shouldersurfing/api/plugin/IShoulderSurfingRegistrar.java
@@ -5,6 +5,7 @@ import java.util.stream.StreamSupport;
 
 import com.github.exopandora.shouldersurfing.api.callback.IAdaptiveItemCallback;
 
+import com.github.exopandora.shouldersurfing.api.callback.ITargetCameraOffsetCallback;
 import net.minecraft.world.item.ItemStack;
 
 public interface IShoulderSurfingRegistrar
@@ -15,4 +16,6 @@ public interface IShoulderSurfingRegistrar
 	{
 		return this.registerAdaptiveItemCallback((minecraft, entity) -> StreamSupport.stream(entity.getHandSlots().spliterator(), false).anyMatch(predicate));
 	}
+	
+	IShoulderSurfingRegistrar registerTargetCameraOffsetCallback(ITargetCameraOffsetCallback targetCameraOffsetCallback);
 }
