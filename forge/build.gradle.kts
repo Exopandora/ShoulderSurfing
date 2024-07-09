83ead3607fd819891d11ab8a813a894942a9f2f5
Exopandora <mkonrad98@outlook.com>
2024-07-09 11:49:01 +0200

    Improve buildscript

--- forge/build.gradle.kts
+++ forge/build.gradle.kts
@@ -84,12 +84,12 @@ dependencies {
 	implementation(fg.deobf(libs.jade.forge.get()))
 }
 
-tasks.named<JavaCompile>("compileJava").configure {
+tasks.named<JavaCompile>("compileJava") {
 	source(project(":api").sourceSets.main.get().allSource)
 	source(project(":common").sourceSets.main.get().allSource)
 }
 
-tasks.named<ProcessResources>("processResources").configure {
+tasks.named<ProcessResources>("processResources") {
 	from(project(":common").sourceSets.main.get().resources)
 	
 	val properties = mapOf(
@@ -109,7 +109,7 @@ tasks.named<ProcessResources>("processResources").configure {
 	}
 }
 
-tasks.register<Jar>("apiJar").configure {
+tasks.register<Jar>("apiJar") {
 	from(project(":api").sourceSets.main.get().output)
 	from(project(":api").sourceSets.main.get().allSource)
 	archiveClassifier = "API"
