32668c2a7c7626c9dffdc39cbef0b1b1289ef3d8
Exopandora <mkonrad98@outlook.com>
2024-07-19 13:48:58 +0200

    Simplify mixin compatibility plugins

--- forge/src/main/java/com/github/exopandora/shouldersurfing/forge/compat/ShoulderOculusCompatMixinPlugin.java
+++ forge/src/main/java/com/github/exopandora/shouldersurfing/forge/compat/ShoulderOculusCompatMixinPlugin.java
@@ -1,7 +1,8 @@
 package com.github.exopandora.shouldersurfing.forge.compat;
 
-import net.minecraftforge.fml.loading.FMLLoader;
+import com.github.exopandora.shouldersurfing.compat.Mods;
 import org.apache.maven.artifact.versioning.ArtifactVersion;
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 import org.apache.maven.artifact.versioning.VersionRange;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
@@ -36,11 +37,13 @@ public class ShoulderOculusCompatMixinPlugin implements IMixinConfigPlugin
 	{
 		if(this.rules.containsKey(mixinClassName))
 		{
-			Predicate<ArtifactVersion> oculusVersion = this.rules.get(mixinClassName).get();
-			return FMLLoader.getLoadingModList().getMods().stream().anyMatch(info ->
+			String oculusVersion = Mods.OCULUS.getModVersion();
+			
+			if(oculusVersion != null)
 			{
-				return info.getModId().equals("oculus") && oculusVersion.test(info.getVersion());
-			});
+				Predicate<ArtifactVersion> versionPredicate = this.rules.get(mixinClassName).get();
+				return versionPredicate.test(new DefaultArtifactVersion(oculusVersion));
+			}
 		}
 		
 		return false;
