8ef9b6fae0771b5588770bd55448fc6f62be0fc9
Exopandora <mkonrad98@outlook.com>
2024-07-19 13:58:20 +0200

    Update formatting

--- neoforge/src/main/java/com/github/exopandora/shouldersurfing/neoforge/event/ClientEventHandler.java
+++ neoforge/src/main/java/com/github/exopandora/shouldersurfing/neoforge/event/ClientEventHandler.java
@@ -27,7 +27,7 @@ public class ClientEventHandler
 		{
 			ShoulderSurfingImpl.getInstance().getCrosshairRenderer().offsetCrosshair(event.getGuiGraphics().pose(), Minecraft.getInstance().getWindow());
 		}
-		//Using BOSS_OVERLAY to pop matrix, because when CROSSHAIR is cancelled, it will not fire RenderGuiOverlayEvent.Post and cause a stack overflow
+		// Using BOSS_OVERLAY to pop matrix, because when CROSSHAIR is cancelled, it will not fire RenderGuiOverlayEvent.Post and cause a stack overflow
 		else if(VanillaGuiLayers.BOSS_OVERLAY.equals(event.getName()))
 		{
 			ShoulderSurfingImpl.getInstance().getCrosshairRenderer().clearCrosshairOffset(event.getGuiGraphics().pose());
