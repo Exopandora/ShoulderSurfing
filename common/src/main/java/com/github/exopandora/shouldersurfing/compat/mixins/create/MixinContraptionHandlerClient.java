c0cfcbffc736a649f3e9f97430732cb9102e8425
Exopandora <mkonrad98@outlook.com>
2024-07-22 20:38:41 +0200

    Fix create mod compatibility for forge build

--- common/src/main/java/com/github/exopandora/shouldersurfing/compat/mixins/create/MixinContraptionHandlerClient.java
+++ common/src/main/java/com/github/exopandora/shouldersurfing/compat/mixins/create/MixinContraptionHandlerClient.java
@@ -2,34 +2,28 @@ package com.github.exopandora.shouldersurfing.compat.mixins.create;
 
 import com.github.exopandora.shouldersurfing.api.client.IObjectPicker;
 import com.github.exopandora.shouldersurfing.api.client.ShoulderSurfing;
-import com.github.exopandora.shouldersurfing.api.model.Couple;
 import com.github.exopandora.shouldersurfing.api.model.PickContext;
+import com.simibubi.create.content.contraptions.ContraptionHandlerClient;
+import com.simibubi.create.foundation.utility.Couple;
 import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Pseudo
-@Mixin(targets = "com.simibubi.create.content.contraptions.ContraptionHandlerClient")
+@Mixin(ContraptionHandlerClient.class)
 public class MixinContraptionHandlerClient
 {
-	@ModifyArgs
+	@Inject
 	(
 		method = "getRayInputs",
-		at = @At
-		(
-			value = "INVOKE",
-			target = "com/simibubi/create/foundation/utility/Couple.create(Ljava/lang/Object;Ljava/lang/Object;)Lcom/simibubi/create/foundation/utility/Couple;",
-			remap = false
-		),
+		at = @At("HEAD"),
+		cancellable = true,
 		remap = false
 	)
-	private static void getRayInputs(Args args, LocalPlayer player)
+	private static void getRayInputs(CallbackInfoReturnable<Couple<Vec3>> cir)
 	{
 		if(ShoulderSurfing.getInstance().isShoulderSurfing())
 		{
@@ -37,11 +31,10 @@ public class MixinContraptionHandlerClient
 			Camera camera = minecraft.gameRenderer.getMainCamera();
 			float partialTick = minecraft.getDeltaFrameTime();
 			double interactionRange = minecraft.gameMode.getPickRange();
-			Couple<Vec3> blockTrace = new PickContext.Builder(camera)
+			var blockTrace = new PickContext.Builder(camera)
 				.build()
 				.blockTrace(interactionRange, partialTick);
-			args.set(0, blockTrace.left());
-			args.set(1, blockTrace.right());
+			cir.setReturnValue(Couple.create(blockTrace.left(), blockTrace.right()));
 		}
 	}
 }
