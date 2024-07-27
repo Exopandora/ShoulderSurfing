73021a2be911bfc5c67fd57c2863368895b8f2cc
Exopandora <mkonrad98@outlook.com>
2024-07-27 11:35:48 +0200

    Replace reflection with direct access

--- common/src/main/java/com/github/exopandora/shouldersurfing/compat/plugin/CreateModTargetCameraOffsetCallback.java
+++ common/src/main/java/com/github/exopandora/shouldersurfing/compat/plugin/CreateModTargetCameraOffsetCallback.java
@@ -2,30 +2,14 @@ package com.github.exopandora.shouldersurfing.compat.plugin;
 
 import com.github.exopandora.shouldersurfing.api.callback.ITargetCameraOffsetCallback;
 import com.github.exopandora.shouldersurfing.api.client.IShoulderSurfing;
+import com.simibubi.create.content.trains.CameraDistanceModifier;
 import net.minecraft.world.phys.Vec3;
-
-import java.lang.reflect.Method;
 
 public class CreateModTargetCameraOffsetCallback implements ITargetCameraOffsetCallback
 {
-	private final Method getMultiplierMethod;
-	
-	public CreateModTargetCameraOffsetCallback() throws ClassNotFoundException, NoSuchMethodException, SecurityException
-	{
-		Class<?> cameraDistanceModifierClass = Class.forName("com.simibubi.create.content.trains.CameraDistanceModifier");
-		this.getMultiplierMethod = cameraDistanceModifierClass.getDeclaredMethod("getMultiplier");
-	}
-	
 	@Override
 	public Vec3 post(IShoulderSurfing instance, Vec3 targetOffset, Vec3 defaultOffset)
 	{
-		try
-		{
-			return targetOffset.multiply(1.0D, 1.0D, (float) this.getMultiplierMethod.invoke(null));
-		}
-		catch(Exception e)
-		{
-			return targetOffset;
-		}
+		return targetOffset.multiply(1.0D, 1.0D, CameraDistanceModifier.getMultiplier());
 	}
 }
