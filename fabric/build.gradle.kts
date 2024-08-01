f60d720a9e31b5bf5b81ff60bd4ec9b91a044311
Exopandora <mkonrad98@outlook.com>
2024-08-01 22:54:03 +0200

    Support translating config options

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -102,7 +102,7 @@ tasks.withType<ProcessResources> {
 	
 	inputs.properties(properties)
 	
-	filesMatching(listOf("pack.mcmeta", "fabric.mod.json")) {
+	filesMatching(listOf("pack.mcmeta", "fabric.mod.json", "**/lang/*.json")) {
 		expand(properties)
 	}
 }
