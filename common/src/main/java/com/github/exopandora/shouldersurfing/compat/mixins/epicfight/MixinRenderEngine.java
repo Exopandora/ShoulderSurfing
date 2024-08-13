76740d7eb7a3524911382f76e79a9b32312cdeff
Exopandora <mkonrad98@outlook.com>
2024-08-13 23:25:17 +0200

    Rotate camera in lock on mode of epic fight when camera is coupled

--- common/src/main/java/com/github/exopandora/shouldersurfing/compat/mixins/epicfight/MixinRenderEngine.java
+++ common/src/main/java/com/github/exopandora/shouldersurfing/compat/mixins/epicfight/MixinRenderEngine.java
@@ -1,23 +1,50 @@
 package com.github.exopandora.shouldersurfing.compat.mixins.epicfight;
 
 import com.github.exopandora.shouldersurfing.client.ShoulderSurfingImpl;
+import com.github.exopandora.shouldersurfing.config.Config;
+import net.minecraft.client.Camera;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Pseudo
 @Mixin(targets = "yesman.epicfight.client.events.engine.RenderEngine")
 public class MixinRenderEngine
 {
+	@Redirect
+	(
+		method = "correctCamera",
+		at = @At
+		(
+			value = "INVOKE",
+			target = "net/minecraft/client/Camera.setRotation(FF)V",
+			remap = true
+		),
+		remap = false
+	)
+	private void correctCamera(Camera camera, float yRot, float xRot)
+	{
+		if(!ShoulderSurfingImpl.getInstance().isShoulderSurfing())
+		{
+			((AccessorCamera) camera).invokeSetRotation(yRot, xRot);
+		}
+		else if(!Config.CLIENT.isCameraDecoupled())
+		{
+			ShoulderSurfingImpl.getInstance().getCamera().setXRot(xRot);
+			ShoulderSurfingImpl.getInstance().getCamera().setYRot(yRot);
+		}
+	}
+	
 	@Inject
 	(
 		method = "correctCamera",
 		at = @At
 		(
 			value = "INVOKE",
-			target = "net/minecraft/client/CameraType.isMirrored()Z",
+			target = "net/minecraft/client/CameraType.isFirstPerson()Z",
 			remap = true
 		),
 		cancellable = true,
