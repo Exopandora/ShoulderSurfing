1d1834e7f0f57fb22148643a9f0449f41a43ec31
Estecka <estecka@galacsys.com>
2024-10-19 01:42:51 +0200

    Wrap the render call instead of injecting it.

--- fabric/src/main/java/com/github/exopandora/shouldersurfing/fabric/mixins/MixinGui.java
+++ fabric/src/main/java/com/github/exopandora/shouldersurfing/fabric/mixins/MixinGui.java
@@ -6,11 +6,15 @@ import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.LayeredDraw.Layer;
+
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Gui.class)
@@ -18,31 +22,40 @@ public class MixinGui
 {
 	@Shadow
 	private @Final Minecraft minecraft;
-	
-	@Inject
-	(
-		method = "renderCrosshair",
-		at = @At("HEAD"),
-		cancellable = true
+	@Shadow
+	private void renderCrosshair(GuiGraphics guiGraphics, DeltaTracker deltaTracker){ throw new AssertionError(); }
+
+	/**
+	 * Targets `new LayeredDraw().add(this::renderCrosshair)`
+	 */
+	@ModifyArg(
+		method="<init>",
+		at = @At(value="INVOKE", target="net/minecraft/client/gui/LayeredDraw.add(Lnet/minecraft/client/gui/LayeredDraw$Layer;)Lnet/minecraft/client/gui/LayeredDraw;", ordinal = 1),
+		index = 0
 	)
-	private void offsetCrosshair(GuiGraphics guiGraphics, DeltaTracker deltaTracker, CallbackInfo ci)
-	{
+	private Layer WrapRenderCrosshair(Layer original){
+		return this::renderCrosshairWrapper;
+	}
+
+	@Unique
+	private void renderCrosshairWrapper(GuiGraphics guiGraphics, DeltaTracker deltaTracker){
 		CrosshairRenderer crosshairRenderer = ShoulderSurfingImpl.getInstance().getCrosshairRenderer();
+
+		// Draw primary crosshair
 		crosshairRenderer.preRenderCrosshair(guiGraphics.pose(), this.minecraft.getWindow());
-		
+
 		if(!crosshairRenderer.doRenderCrosshair())
 		{
-			ci.cancel();
+			return;
 		}
-	}
-	
-	@Inject
-	(
-		method = "renderCrosshair",
-		at = @At("RETURN")
-	)
-	private void clearCrosshairOffset(GuiGraphics guiGraphics, DeltaTracker deltaTracker, CallbackInfo ci)
-	{
+		this.renderCrosshair(guiGraphics, deltaTracker);
 		ShoulderSurfingImpl.getInstance().getCrosshairRenderer().postRenderCrosshair(guiGraphics.pose());
+
+
+		// Draw secondary crosshair
+		// if (crosshairRenderer.isCrosshairDynamic(minecraft.getCameraEntity())){
+
+		// }
 	}
+
 }
