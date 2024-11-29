991f7e250b9c1a115fddf9e36410da8edb6541c9
Exopandora <mkonrad98@outlook.com>
2024-11-29 20:04:46 +0100

    Improve compatibility for dynamic crosshair. Closes #256

--- neoforge/src/main/java/com/github/exopandora/shouldersurfing/neoforge/event/ClientEventHandler.java
+++ neoforge/src/main/java/com/github/exopandora/shouldersurfing/neoforge/event/ClientEventHandler.java
@@ -1,11 +1,14 @@
 package com.github.exopandora.shouldersurfing.neoforge.event;
 
+import com.github.exopandora.shouldersurfing.ShoulderSurfingCommon;
 import com.github.exopandora.shouldersurfing.client.CrosshairRenderer;
 import com.github.exopandora.shouldersurfing.client.ShoulderSurfingImpl;
 import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.event.MovementInputUpdateEvent;
+import net.neoforged.neoforge.client.event.RegisterGuiLayersEvent;
 import net.neoforged.neoforge.client.event.RenderGuiLayerEvent;
 import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
 import net.neoforged.neoforge.client.gui.VanillaGuiLayers;
@@ -24,24 +27,28 @@ public class ClientEventHandler
 	@SubscribeEvent
 	public static void preRenderGuiOverlayEvent(RenderGuiLayerEvent.Pre event)
 	{
-		if(VanillaGuiLayers.CROSSHAIR.equals(event.getName()))
+		if(VanillaGuiLayers.CROSSHAIR.equals(event.getName()) && !ShoulderSurfingImpl.getInstance().getCrosshairRenderer().doRenderCrosshair())
+		{
+			event.setCanceled(true);
+		}
+	}
+	
+	@SubscribeEvent
+	public static void registerGuiOverlaysEvent(RegisterGuiLayersEvent event)
+	{
+		event.registerBelow(VanillaGuiLayers.CROSSHAIR, ResourceLocation.fromNamespaceAndPath(ShoulderSurfingCommon.MOD_ID, "pre_crosshair"), (guiGraphics, deltaTracker) ->
 		{
 			CrosshairRenderer crosshairRenderer = ShoulderSurfingImpl.getInstance().getCrosshairRenderer();
 			
 			if(crosshairRenderer.doRenderCrosshair())
 			{
-				crosshairRenderer.preRenderCrosshair(event.getGuiGraphics());
+				crosshairRenderer.preRenderCrosshair(guiGraphics);
 			}
-			else
-			{
-				event.setCanceled(true);
-			}
-		}
-		// Using HOTBAR to pop matrix, because when CROSSHAIR is cancelled, it will not fire RenderGuiOverlayEvent.Post and cause a stack overflow
-		else if(VanillaGuiLayers.HOTBAR.equals(event.getName()))
+		});
+		event.registerAbove(VanillaGuiLayers.CROSSHAIR, ResourceLocation.fromNamespaceAndPath(ShoulderSurfingCommon.MOD_ID, "post_crosshair"), (guiGraphics, deltaTracker) ->
 		{
-			ShoulderSurfingImpl.getInstance().getCrosshairRenderer().postRenderCrosshair(event.getGuiGraphics());
-		}
+			ShoulderSurfingImpl.getInstance().getCrosshairRenderer().postRenderCrosshair(guiGraphics);
+		});
 	}
 	
 	@SubscribeEvent
