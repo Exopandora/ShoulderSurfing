0e6b12c7ba610746d524dad764109db6961dbaf7
Exopandora <mkonrad98@outlook.com>
2025-01-13 23:40:44 +0100

    Refactor perspective change

--- common/src/main/java/com/github/exopandora/shouldersurfing/mixins/MixinGameSettings.java
+++ common/src/main/java/com/github/exopandora/shouldersurfing/mixins/MixinGameSettings.java
@@ -1,10 +1,11 @@
 package com.github.exopandora.shouldersurfing.mixins;
 
+import com.github.exopandora.shouldersurfing.api.model.Perspective;
 import com.github.exopandora.shouldersurfing.client.ShoulderSurfingImpl;
 import com.github.exopandora.shouldersurfing.config.Config;
 import com.github.exopandora.shouldersurfing.mixinducks.GameSettingsDuck;
-import net.minecraft.client.settings.PointOfView;
 import net.minecraft.client.GameSettings;
+import net.minecraft.client.settings.PointOfView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -21,13 +22,16 @@ public abstract class MixinGameSettings implements GameSettingsDuck
 	@Inject
 	(
 		at = @At("HEAD"),
-		method = "setCameraType"
+		method = "setCameraType",
+		cancellable = true
 	)
 	public void setCameraType(PointOfView cameraType, CallbackInfo ci)
 	{
 		if(cameraType != this.cameraType)
 		{
-			ShoulderSurfingImpl.getInstance().setShoulderSurfing(Config.CLIENT.replaceDefaultPerspective() && cameraType.equals(PointOfView.THIRD_PERSON_BACK));
+			Perspective newPerspective = Perspective.of(cameraType, Config.CLIENT.replaceDefaultPerspective() && cameraType == PointOfView.THIRD_PERSON_BACK);
+			ShoulderSurfingImpl.getInstance().changePerspective(newPerspective);
+			ci.cancel();
 		}
 	}
 	
