6943bd74018c5de1648df3cd2c2e6586b83d6d35
Exopandora <mkonrad98@outlook.com>
2025-01-26 16:01:09 +0100

    Add config option for whether to orient the camera on teleport. Closes #267

--- common/src/main/java/com/github/exopandora/shouldersurfing/config/Config.java
+++ common/src/main/java/com/github/exopandora/shouldersurfing/config/Config.java
@@ -104,6 +104,7 @@ public class Config
 		private final DoubleValue centerCameraWhenLookingDownAngle;
 		private final BooleanValue dynamicallyAdjustOffsets;
 		private final BooleanValue isCameraDecoupled;
+		private final BooleanValue orientCameraOnTeleport;
 		
 		private final BooleanValue replaceDefaultPerspective;
 		private final BooleanValue isFirstPersonEnabled;
@@ -435,6 +436,11 @@ public class Config
 				.comment("Whether or not to decouple the camera rotation from the player rotation.")
 				.translation(MOD_ID + ".configuration.camera.decoupled_camera")
 				.define("decoupled_camera", true);
+			
+			this.orientCameraOnTeleport = builder
+				.comment("Whether or not to orient the camera rotation when the player is teleported. This includes passenger (dis-)mounting and traveling through portals.")
+				.translation(MOD_ID + ".configuration.camera.orient_camera_on_teleport")
+				.define("orient_camera_on_teleport", true);
 			
 			builder.pop();
 			builder.push("perspective");
@@ -1072,6 +1078,12 @@ public class Config
 		public boolean isCameraDecoupled()
 		{
 			return this.isCameraDecoupled.get();
+		}
+		
+		@Override
+		public boolean doOrientCameraOnTeleport()
+		{
+			return this.orientCameraOnTeleport.get();
 		}
 		
 		@Override
--- common/src/main/java/com/github/exopandora/shouldersurfing/mixins/MixinClientPacketListener.java
+++ common/src/main/java/com/github/exopandora/shouldersurfing/mixins/MixinClientPacketListener.java
@@ -2,6 +2,7 @@ package com.github.exopandora.shouldersurfing.mixins;
 
 import com.github.exopandora.shouldersurfing.client.ShoulderSurfingCamera;
 import com.github.exopandora.shouldersurfing.client.ShoulderSurfingImpl;
+import com.github.exopandora.shouldersurfing.config.Config;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientCommonPacketListenerImpl;
 import net.minecraft.client.multiplayer.ClientPacketListener;
@@ -62,7 +63,7 @@ public abstract class MixinClientPacketListener extends ClientCommonPacketListen
 	{
 		ShoulderSurfingImpl instance = ShoulderSurfingImpl.getInstance();
 		
-		if(instance.isShoulderSurfing())
+		if(instance.isShoulderSurfing() && Config.CLIENT.doOrientCameraOnTeleport())
 		{
 			Player player = this.minecraft.player;
 			boolean isRelativeXRot = packet.relatives().contains(Relative.X_ROT);
