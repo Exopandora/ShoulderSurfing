57562f831eed42d69095ee14eb00386e9c8166ec
Exopandora <mkonrad98@outlook.com>
2025-04-07 19:09:06 +0200

    Add config option for camera lock-on for epic fight integration in decoupled mode

--- common/src/main/java/com/github/exopandora/shouldersurfing/config/Config.java
+++ common/src/main/java/com/github/exopandora/shouldersurfing/config/Config.java
@@ -147,6 +147,8 @@ public class Config
 		
 		private final BooleanValue centerPlayerSounds;
 		
+		private final BooleanValue epicFightDecoupledCameraLockOn;
+		
 		private boolean requiresSaving = false;
 		
 		public ClientConfig(ModConfigSpec.Builder builder)
@@ -683,6 +685,16 @@ public class Config
 				.define("center_player_sounds", false);
 			
 			builder.pop();
+			builder.push("integrations");
+			builder.push("epicfight");
+			
+			this.epicFightDecoupledCameraLockOn = builder
+				.comment("Whether to allow target lock-on when camera is decoupled.")
+				.translation(MOD_ID + ".configuration.integrations.epicfight.decoupled_camera_lock_on")
+				.define("decoupled_camera_lock_on", false);
+			
+			builder.pop();
+			builder.pop();
 		}
 		
 		@Override
@@ -1198,6 +1210,12 @@ public class Config
 		public double getPlayerYRotFollowAngleLimit()
 		{
 			return this.playerYRotFollowAngleLimit.get();
+		}
+		
+		@Override
+		public boolean getEpicFightDecoupledCameraLockOn()
+		{
+			return this.epicFightDecoupledCameraLockOn.get();
 		}
 		
 		public void adjustCameraLeft()
