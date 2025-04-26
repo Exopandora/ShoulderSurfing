572903ae1d19e1744eba4f6b759f7ff5334ff760
Thiago Oliveira Santos <tos.oliveira@gmail.com>
2025-04-26 12:01:36 -0300

    Add formatting pipeline for PR validation (#296)

--- api/src/main/java/com/github/exopandora/shouldersurfing/api/client/IClientConfig.java
+++ api/src/main/java/com/github/exopandora/shouldersurfing/api/client/IClientConfig.java
@@ -13,23 +13,33 @@ import java.util.List;
 public interface IClientConfig
 {
 	double getOffsetX();
+	
 	double getOffsetY();
+	
 	double getOffsetZ();
 	
 	double getMinOffsetX();
+	
 	double getMinOffsetY();
+	
 	double getMinOffsetZ();
 	
 	double getMaxOffsetX();
+	
 	double getMaxOffsetY();
+	
 	double getMaxOffsetZ();
 	
 	boolean isUnlimitedOffsetX();
+	
 	boolean isUnlimitedOffsetY();
+	
 	boolean isUnlimitedOffsetZ();
 	
 	double getPassengerOffsetXMultiplier();
+	
 	double getPassengerOffsetYMultiplier();
+	
 	double getPassengerOffsetZMultiplier();
 	
 	default Vec3 getPassengerOffsetMultipliers()
@@ -38,7 +48,9 @@ public interface IClientConfig
 	}
 	
 	double getSprintOffsetXMultiplier();
+	
 	double getSprintOffsetYMultiplier();
+	
 	double getSprintOffsetZMultiplier();
 	
 	default Vec3 getSprintOffsetMultipliers()
@@ -47,7 +59,9 @@ public interface IClientConfig
 	}
 	
 	double getAimingOffsetXMultiplier();
+	
 	double getAimingOffsetYMultiplier();
+	
 	double getAimingOffsetZMultiplier();
 	
 	default Vec3 getAimingOffsetMultipliers()
@@ -56,7 +70,9 @@ public interface IClientConfig
 	}
 	
 	double getFallFlyingOffsetXMultiplier();
+	
 	double getFallFlyingOffsetYMultiplier();
+	
 	double getFallFlyingOffsetZMultiplier();
 	
 	default Vec3 getFallFlyingMultipliers()
@@ -65,7 +81,9 @@ public interface IClientConfig
 	}
 	
 	double getClimbingOffsetXMultiplier();
+	
 	double getClimbingOffsetYMultiplier();
+	
 	double getClimbingOffsetZMultiplier();
 	
 	default Vec3 getClimbingMultipliers()
@@ -74,7 +92,9 @@ public interface IClientConfig
 	}
 	
 	double getPassengerOffsetXModifier();
+	
 	double getPassengerOffsetYModifier();
+	
 	double getPassengerOffsetZModifier();
 	
 	default Vec3 getPassengerOffsetModifiers()
@@ -83,7 +103,9 @@ public interface IClientConfig
 	}
 	
 	double getSprintOffsetXModifier();
+	
 	double getSprintOffsetYModifier();
+	
 	double getSprintOffsetZModifier();
 	
 	default Vec3 getSprintOffsetModifiers()
@@ -92,7 +114,9 @@ public interface IClientConfig
 	}
 	
 	double getAimingOffsetXModifier();
+	
 	double getAimingOffsetYModifier();
+	
 	double getAimingOffsetZModifier();
 	
 	default Vec3 getAimingOffsetModifiers()
@@ -101,7 +125,9 @@ public interface IClientConfig
 	}
 	
 	double getFallFlyingOffsetXModifier();
+	
 	double getFallFlyingOffsetYModifier();
+	
 	double getFallFlyingOffsetZModifier();
 	
 	default Vec3 getFallFlyingOffsetModifiers()
@@ -110,7 +136,9 @@ public interface IClientConfig
 	}
 	
 	double getClimbingOffsetXModifier();
+	
 	double getClimbingOffsetYModifier();
+	
 	double getClimbingOffsetZModifier();
 	
 	default Vec3 getClimbingOffsetModifiers()
@@ -133,7 +161,9 @@ public interface IClientConfig
 	}
 	
 	boolean isFirstPersonEnabled();
+	
 	boolean isThirdPersonFrontEnabled();
+	
 	boolean isThirdPersonBackEnabled();
 	
 	Perspective getDefaultPerspective();
@@ -155,13 +185,17 @@ public interface IClientConfig
 	boolean isPlayerTransparencyEnabled();
 	
 	TurningMode getTurningModeWhenUsingItem();
+	
 	TurningMode getTurningModeWhenAttacking();
+	
 	TurningMode getTurningModeWhenInteracting();
+	
 	TurningMode getTurningModeWhenPicking();
 	
 	int getTurningLockTime();
 	
 	PickOrigin getEntityPickOrigin();
+	
 	PickOrigin getBlockPickOrigin();
 	
 	PickVector getPickVector();
@@ -177,18 +211,25 @@ public interface IClientConfig
 	double getCustomRaytraceDistance();
 	
 	List<? extends String> getAdaptiveCrosshairHoldItems();
+	
 	List<? extends String> getAdaptiveCrosshairUseItems();
+	
 	List<? extends String> getAdaptiveCrosshairHoldItemProperties();
+	
 	List<? extends String> getAdaptiveCrosshairUseItemProperties();
 	
 	boolean getShowObstructionCrosshair();
+	
 	boolean showObstructionIndicatorWhenAiming();
+	
 	int getObstructionIndicatorMinDistanceToCrosshair();
+	
 	double getObstructionIndicatorMaxDistanceToObstruction();
 	
 	boolean doCenterPlayerSounds();
 	
 	boolean shouldPlayerXRotFollowCamera();
+	
 	boolean shouldPlayerYRotFollowCamera();
 	
 	double getPlayerYRotFollowAngleLimit();
