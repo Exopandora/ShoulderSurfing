e277336a3cf5b15b00a069569bcc15d0c7bd9bef
Thiago Oliveira Santos <tos.oliveira@gmail.com>
2025-04-26 12:01:36 -0300

    Add formatting pipeline for PR validation (#296)

--- api/src/main/java/com/github/exopandora/shouldersurfing/api/client/IShoulderSurfingCamera.java
+++ api/src/main/java/com/github/exopandora/shouldersurfing/api/client/IShoulderSurfingCamera.java
@@ -7,12 +7,16 @@ public interface IShoulderSurfingCamera
 	double getCameraDistance();
 	
 	Vec3 getOffset();
+	
 	Vec3 getRenderOffset();
+	
 	Vec3 getTargetOffset();
 	
 	float getXRot();
+	
 	void setXRot(float xRot);
 	
 	float getYRot();
+	
 	void setYRot(float yRot);
 }
