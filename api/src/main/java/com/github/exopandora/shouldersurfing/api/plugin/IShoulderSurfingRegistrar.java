c9ae8d482598197d419f80c61d30091429df07ee
Thiago Oliveira Santos <tos.oliveira@gmail.com>
2025-04-27 11:15:57 -0300

    Add api callback to force camera coupling (#295)

--- api/src/main/java/com/github/exopandora/shouldersurfing/api/plugin/IShoulderSurfingRegistrar.java
+++ api/src/main/java/com/github/exopandora/shouldersurfing/api/plugin/IShoulderSurfingRegistrar.java
@@ -1,16 +1,18 @@
 package com.github.exopandora.shouldersurfing.api.plugin;
 
-import java.util.function.Predicate;
-import java.util.stream.StreamSupport;
-
 import com.github.exopandora.shouldersurfing.api.callback.IAdaptiveItemCallback;
-
+import com.github.exopandora.shouldersurfing.api.callback.ICameraCouplingCallback;
 import com.github.exopandora.shouldersurfing.api.callback.ITargetCameraOffsetCallback;
 import net.minecraft.world.item.ItemStack;
+
+import java.util.function.Predicate;
+import java.util.stream.StreamSupport;
 
 public interface IShoulderSurfingRegistrar
 {
 	IShoulderSurfingRegistrar registerAdaptiveItemCallback(IAdaptiveItemCallback adaptiveItemCallback);
+	
+	IShoulderSurfingRegistrar registerCameraCouplingCallback(ICameraCouplingCallback cameraCouplingCallback);
 	
 	default IShoulderSurfingRegistrar registerAdaptiveItemCallback(Predicate<ItemStack> predicate)
 	{
