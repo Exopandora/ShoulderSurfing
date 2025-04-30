5a87559f3eb29a9853544ff57e829c4a879df48c
Thiago Oliveira Santos <tos.oliveira@gmail.com>
2025-04-30 19:06:22 -0300

    Add build and publishing pipelines (#298)

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -134,7 +134,7 @@ publishMods {
 	
 	curseforge {
 		projectId = curseProjectId
-		accessToken = findProperty("curse_api_key").toString()
+		accessToken = System.getenv("CURSE_API_KEY")
 		minecraftVersions.set(compatibleVersions)
 		javaVersions.add(JavaVersion.toVersion(javaVersion))
 		clientRequired = true
@@ -145,7 +145,7 @@ publishMods {
 	
 	modrinth {
 		projectId = modrinthProjectId
-		accessToken = findProperty("modrinth_api_key").toString()
+		accessToken = System.getenv("MODRINTH_API_KEY")
 		minecraftVersions.set(compatibleVersions)
 		requires("fabric-api", "forge-config-api-port")
 		incompatible("better-third-person", "nimble", "valkyrien-skies")
