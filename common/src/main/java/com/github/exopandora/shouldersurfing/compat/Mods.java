adeda9a1380558781b1774c7d2c6337169e07d78
Exopandora <mkonrad98@outlook.com>
2025-05-09 19:22:09 +0200

    Fix compatibility with wildfire gender. Closes #306

--- common/src/main/java/com/github/exopandora/shouldersurfing/compat/Mods.java
+++ common/src/main/java/com/github/exopandora/shouldersurfing/compat/Mods.java
@@ -15,7 +15,8 @@ public enum Mods
 	EPIC_FIGHT,
 	SKIN_LAYERS,
 	THE_ONE_PROBE,
-	TSLAT_ENTITY_STATUS;
+	TSLAT_ENTITY_STATUS,
+	WILDFIRE_GENDER;
 	
 	private static final Map<Mods, @Nullable String> MODS_TO_VERSION = new HashMap<Mods, String>();
 	
