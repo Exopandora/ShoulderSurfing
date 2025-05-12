d8f5d1d2eb72b8f5dce1e5ca9592ce4ccf6442eb
Exopandora <mkonrad98@outlook.com>
2025-05-12 20:41:42 +0200

    Add configurable camera sway

--- forge/src/main/java/com/github/exopandora/shouldersurfing/forge/event/ClientEventHandler.java
+++ forge/src/main/java/com/github/exopandora/shouldersurfing/forge/event/ClientEventHandler.java
@@ -1,8 +1,11 @@
 package com.github.exopandora.shouldersurfing.forge.event;
 
 import com.github.exopandora.shouldersurfing.client.ShoulderSurfingImpl;
+import com.github.exopandora.shouldersurfing.mixinducks.CameraDuck;
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.Mth;
 import net.minecraftforge.client.event.MovementInputUpdateEvent;
+import net.minecraftforge.client.event.ViewportEvent;
 import net.minecraftforge.event.TickEvent.ClientTickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -22,5 +25,11 @@ public class ClientEventHandler
 	{
 		ShoulderSurfingImpl.getInstance().getInputHandler().updateMovementInput(event.getInput());
 		ShoulderSurfingImpl.getInstance().updatePlayerRotations();
+	}
+	
+	@SubscribeEvent
+	public static void computeCameraAnglesEvent(ViewportEvent.ComputeCameraAngles event)
+	{
+		event.setRoll(event.getRoll() - ((CameraDuck) event.getCamera()).shouldersurfing$getZRot());
 	}
 }
