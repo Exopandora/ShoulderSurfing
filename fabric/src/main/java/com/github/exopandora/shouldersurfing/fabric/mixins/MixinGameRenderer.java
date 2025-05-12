efd43ffb1e784fb4a2f39a4e19dea4f954899a38
Exopandora <mkonrad98@outlook.com>
2025-05-12 20:41:42 +0200

    Add configurable camera sway

---
+++ fabric/src/main/java/com/github/exopandora/shouldersurfing/fabric/mixins/MixinGameRenderer.java
@@ -0,0 +1,36 @@
+package com.github.exopandora.shouldersurfing.fabric.mixins;
+
+import com.github.exopandora.shouldersurfing.mixinducks.CameraDuck;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import net.minecraft.client.Camera;
+import net.minecraft.client.renderer.GameRenderer;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameRenderer.class)
+public class MixinGameRenderer
+{
+	@Shadow
+	private @Final Camera mainCamera;
+	
+	@Inject
+	(
+		method = "renderLevel",
+		at = @At
+		(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/Camera;setup(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/world/entity/Entity;ZZF)V",
+			shift = Shift.AFTER
+		)
+	)
+	private void renderLevel(float partialTick, long nanos, PoseStack poseStack, CallbackInfo ci)
+	{
+		poseStack.mulPose(Axis.ZP.rotationDegrees(((CameraDuck) this.mainCamera).shouldersurfing$getZRot()));
+	}
+}
