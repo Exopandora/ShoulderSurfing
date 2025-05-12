3d1d3d8edb77a8fe89bd07fd6f4df779559163ba
Exopandora <mkonrad98@outlook.com>
2025-05-12 20:41:42 +0200

    Add configurable camera sway

---
+++ neoforge/src/main/java/com/github/exopandora/shouldersurfing/neoforge/mixins/MixinCamera.java
@@ -0,0 +1,29 @@
+package com.github.exopandora.shouldersurfing.neoforge.mixins;
+
+import com.github.exopandora.shouldersurfing.mixinducks.CameraDuck;
+import net.minecraft.client.Camera;
+import net.minecraft.util.Mth;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(Camera.class)
+public abstract class MixinCamera implements CameraDuck
+{
+	@ModifyArg
+	(
+		method = "setRotation(FFF)V",
+		at = @At
+		(
+			value = "INVOKE",
+			target = "Lorg/joml/Quaternionf;rotationYXZ(FFF)Lorg/joml/Quaternionf;",
+			remap = false
+		),
+		remap = false,
+		index = 2
+	)
+	private float rotationYXZ(float zRot)
+	{
+		return this.shouldersurfing$getZRot() * -Mth.RAD_TO_DEG + zRot;
+	}
+}
