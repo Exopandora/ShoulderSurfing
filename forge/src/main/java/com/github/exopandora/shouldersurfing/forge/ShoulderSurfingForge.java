b5f5db25117a5bff0f856489ffe03fcb9d28a197
Exopandora <mkonrad98@outlook.com>
2025-06-19 22:53:30 +0200

    Remove blank lines

--- forge/src/main/java/com/github/exopandora/shouldersurfing/forge/ShoulderSurfingForge.java
+++ forge/src/main/java/com/github/exopandora/shouldersurfing/forge/ShoulderSurfingForge.java
@@ -38,7 +38,6 @@ public class ShoulderSurfingForge
 	{
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		ModLoadingContext modLoadingContext = ModLoadingContext.get();
-		
 		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () ->
 		{
 			modEventBus.addListener(this::clientSetup);
@@ -47,7 +46,6 @@ public class ShoulderSurfingForge
 			modEventBus.addListener(this::modConfigReloadingEvent);
 			modLoadingContext.registerConfig(Type.CLIENT, Config.CLIENT_SPEC);
 		});
-		
 		modLoadingContext.registerExtensionPoint(DisplayTest.class, () -> new DisplayTest(() -> "ANY", (remote, isServer) -> true));
 	}
 	
