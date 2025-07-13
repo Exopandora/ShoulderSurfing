37bc15943c2430cba93cb4bcb92095dfa9185ecd
Exopandora <mkonrad98@outlook.com>
2025-07-13 22:55:05 +0200

    Dynamically generate callback proxies

--- common/src/main/java/com/github/exopandora/shouldersurfing/plugin/ShoulderSurfingRegistrar.java
+++ common/src/main/java/com/github/exopandora/shouldersurfing/plugin/ShoulderSurfingRegistrar.java
@@ -4,17 +4,14 @@ import com.github.exopandora.shouldersurfing.api.callback.IAdaptiveItemCallback;
 import com.github.exopandora.shouldersurfing.api.callback.ICameraCouplingCallback;
 import com.github.exopandora.shouldersurfing.api.callback.ICameraEntityTransparencyCallback;
 import com.github.exopandora.shouldersurfing.api.callback.ITargetCameraOffsetCallback;
-import com.github.exopandora.shouldersurfing.api.client.IShoulderSurfing;
 import com.github.exopandora.shouldersurfing.api.plugin.IShoulderSurfingRegistrar;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.phys.Vec3;
 
+import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.concurrent.Callable;
+import java.util.Set;
 
 public class ShoulderSurfingRegistrar implements IShoulderSurfingRegistrar
 {
@@ -36,36 +33,32 @@ public class ShoulderSurfingRegistrar implements IShoulderSurfingRegistrar
 	@Override
 	public IShoulderSurfingRegistrar registerAdaptiveItemCallback(IAdaptiveItemCallback adaptiveItemCallback)
 	{
-		this.checkState();
-		IAdaptiveItemCallback wrapper = new AdaptiveItemCallbackWrapper(this.activePluginContext, adaptiveItemCallback);
-		this.adaptiveItemCallbacks.add(wrapper);
-		return this;
+		return this.registerCallback(this.adaptiveItemCallbacks, adaptiveItemCallback, IAdaptiveItemCallback.class);
 	}
 	
 	@Override
 	public IShoulderSurfingRegistrar registerCameraCouplingCallback(ICameraCouplingCallback cameraCouplingCallback)
 	{
-		this.checkState();
-		ICameraCouplingCallback wrapper = new CameraCouplingCallbackWrapper(this.activePluginContext, cameraCouplingCallback);
-		this.cameraCouplingCallbacks.add(wrapper);
-		return this;
+		return this.registerCallback(this.cameraCouplingCallbacks, cameraCouplingCallback, ICameraCouplingCallback.class);
 	}
 	
 	@Override
 	public IShoulderSurfingRegistrar registerTargetCameraOffsetCallback(ITargetCameraOffsetCallback targetCameraOffsetCallback)
 	{
-		this.checkState();
-		ITargetCameraOffsetCallback wrapper = new TargetCameraOffsetCallbackWrapper(this.activePluginContext, targetCameraOffsetCallback);
-		this.targetCameraOffsetCallbacks.add(wrapper);
-		return this;
+		return this.registerCallback(this.targetCameraOffsetCallbacks, targetCameraOffsetCallback, ITargetCameraOffsetCallback.class);
 	}
 	
 	@Override
 	public IShoulderSurfingRegistrar registerCameraEntityTransparencyCallback(ICameraEntityTransparencyCallback cameraEntityTransparencyCallback)
 	{
+		return this.registerCallback(this.cameraEntityTransparencyCallbacks, cameraEntityTransparencyCallback, ICameraEntityTransparencyCallback.class);
+	}
+	
+	private <T> IShoulderSurfingRegistrar registerCallback(List<T> registry, T callback, Class<T> klass)
+	{
 		this.checkState();
-		ICameraEntityTransparencyCallback wrapper = new CameraEntityTransparencyCallbackWrapper(this.activePluginContext, cameraEntityTransparencyCallback);
-		this.cameraEntityTransparencyCallbacks.add(wrapper);
+		T proxy = createProxy(this.activePluginContext, callback, klass);
+		registry.add(proxy);
 		return this;
 	}
 	
@@ -113,63 +106,14 @@ public class ShoulderSurfingRegistrar implements IShoulderSurfingRegistrar
 		return Collections.unmodifiableList(this.cameraEntityTransparencyCallbacks);
 	}
 	
-	private record AdaptiveItemCallbackWrapper(PluginContext context, IAdaptiveItemCallback delegate) implements IAdaptiveItemCallback
+	@SuppressWarnings("unchecked")
+	private static <T> T createProxy(PluginContext context, T callback, Class<T> klass)
 	{
-		@Override
-		public boolean isHoldingAdaptiveItem(Minecraft minecraft, LivingEntity entity)
-		{
-			return wrapCallback(this.context, () -> this.delegate.isHoldingAdaptiveItem(minecraft, entity));
-		}
-	}
-	
-	private record CameraCouplingCallbackWrapper(PluginContext context, ICameraCouplingCallback delegate) implements ICameraCouplingCallback
-	{
-		@Override
-		public boolean isForcingCameraCoupling(Minecraft minecraft)
-		{
-			return wrapCallback(this.context, () -> this.delegate.isForcingCameraCoupling(minecraft));
-		}
-	}
-	
-	private record TargetCameraOffsetCallbackWrapper(PluginContext context, ITargetCameraOffsetCallback delegate) implements ITargetCameraOffsetCallback
-	{
-		@Override
-		public Vec3 pre(IShoulderSurfing instance, Vec3 targetOffset, Vec3 defaultOffset)
-		{
-			return wrapCallback(this.context, () -> this.delegate.pre(instance, targetOffset, defaultOffset));
-		}
-		
-		@Override
-		public Vec3 post(IShoulderSurfing instance, Vec3 targetOffset, Vec3 defaultOffset)
-		{
-			return wrapCallback(this.context, () -> this.delegate.post(instance, targetOffset, defaultOffset));
-		}
-	}
-	
-	private record CameraEntityTransparencyCallbackWrapper(PluginContext context, ICameraEntityTransparencyCallback delegate) implements ICameraEntityTransparencyCallback
-	{
-		@Override
-		public float getCameraEntityAlpha(IShoulderSurfing instance, Entity cameraEntity, float partialTick)
-		{
-			return wrapCallback(this.context, () -> this.delegate.getCameraEntityAlpha(instance, cameraEntity, partialTick));
-		}
-	}
-	
-	private static <T> T wrapCallback(PluginContext context, Callable<T> callback)
-	{
-		try
-		{
-			return callback.call();
-		}
-		catch(Throwable t)
-		{
-			throw createExceptionWithContext(context, t);
-		}
-	}
-	
-	private static RuntimeException createExceptionWithContext(PluginContext context, Throwable t)
-	{
-		return new RuntimeException("Shoulder Surfing Reloaded encountered an unexpected error while trying to execute a callback for the plugin provided by " + context.formattedModName() + ". Please report this crash to " + context.formattedModName() + ".", t);
+		Set<Class<?>> interfaces = new LinkedHashSet<Class<?>>(1);
+		interfaces.add(klass);
+		ClassLoader classLoader = ShoulderSurfingRegistrar.class.getClassLoader();
+		CallbackInvocationHandler<T> invocationHandler = new CallbackInvocationHandler<T>(context, callback, interfaces);
+		return (T) Proxy.newProxyInstance(classLoader, interfaces.toArray(Class<?>[]::new), invocationHandler);
 	}
 	
 	public static ShoulderSurfingRegistrar getInstance()
