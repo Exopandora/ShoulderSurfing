e88306e171f9fb7970c2b6ec1398a7dc45bbb2c7
Exopandora <mkonrad98@outlook.com>
2025-07-13 14:55:45 +0200

    Add javadoc to callback interfaces

--- api/src/main/java/com/github/exopandora/shouldersurfing/api/callback/IAdaptiveItemCallback.java
+++ api/src/main/java/com/github/exopandora/shouldersurfing/api/callback/IAdaptiveItemCallback.java
@@ -5,20 +5,41 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.ItemLike;
 
+/**
+ * This callback can be used to implement custom switching behaviour for the adaptive crosshair for any item held.
+ * The final result is calculated from all partial results using a logical OR.
+ */
 public interface IAdaptiveItemCallback
 {
+	/**
+	 * @param minecraft The Minecraft instance
+	 * @param entity The current camera entity
+	 * @return <code>true</code> if the dynamic crosshair should be displayed, <code>false</code> if the static crosshair should be displayed
+	 */
 	boolean isHoldingAdaptiveItem(Minecraft minecraft, LivingEntity entity);
 	
+	/**
+	 * @param items Items to match
+	 * @return An IAdaptiveItemCallback instance that returns <code>true</code> whenever the main-hand matches any of the listed items
+	 */
 	static IAdaptiveItemCallback mainHandMatches(ItemLike... items)
 	{
 		return (minecraft, entity) -> containsItem(entity.getMainHandItem().getItem(), items);
 	}
 	
+	/**
+	 * @param items Items to match
+	 * @return An IAdaptiveItemCallback instance that returns <code>true</code> whenever the off-hand matches any of the listed items
+	 */
 	static IAdaptiveItemCallback offHandMatches(ItemLike... items)
 	{
 		return (minecraft, entity) -> containsItem(entity.getOffhandItem().getItem(), items);
 	}
 	
+	/**
+	 * @param items Items to match
+	 * @return An IAdaptiveItemCallback instance that returns <code>true</code> whenever any hand matches any of the listed items
+	 */
 	static IAdaptiveItemCallback anyHandMatches(ItemLike... items)
 	{
 		return (minecraft, entity) -> containsItem(entity.getMainHandItem().getItem(), items) || containsItem(entity.getOffhandItem().getItem(), items);
--- api/src/main/java/com/github/exopandora/shouldersurfing/api/callback/ICameraEntityTransparencyCallback.java
+++ api/src/main/java/com/github/exopandora/shouldersurfing/api/callback/ICameraEntityTransparencyCallback.java
@@ -3,7 +3,17 @@ package com.github.exopandora.shouldersurfing.api.callback;
 import com.github.exopandora.shouldersurfing.api.client.IShoulderSurfing;
 import net.minecraft.world.entity.Entity;
 
+/**
+ * This callback can be used to implement custom camera entity transparency rules.
+ * The final result is the minimum value from all partial results.
+ */
 public interface ICameraEntityTransparencyCallback
 {
+	/**
+	 * @param instance The IShoulderSurfing instance
+	 * @param cameraEntity The camera entity
+	 * @param partialTick The render partial tick
+	 * @return The alpha value to use for camera entity rendering, ranging from <code>0.0F</code> (invisible) to <code>1.0F</code> (opaque)
+	 */
 	float getCameraEntityAlpha(IShoulderSurfing instance, Entity cameraEntity, float partialTick);
 }
