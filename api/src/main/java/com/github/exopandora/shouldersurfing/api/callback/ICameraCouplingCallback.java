631dc7f682e3c017a0fbf021fa238ed3a7957473
Exopandora <mkonrad98@outlook.com>
2025-07-13 14:55:45 +0200

    Add javadoc to callback interfaces

--- api/src/main/java/com/github/exopandora/shouldersurfing/api/callback/ICameraCouplingCallback.java
+++ api/src/main/java/com/github/exopandora/shouldersurfing/api/callback/ICameraCouplingCallback.java
@@ -2,7 +2,16 @@ package com.github.exopandora.shouldersurfing.api.callback;
 
 import net.minecraft.client.Minecraft;
 
+/**
+ * This callback can be used to implement custom camera coupling rules.
+ * The final result is calculated from all partial results using a logical OR.
+ */
 public interface ICameraCouplingCallback
 {
+	/**
+	 *
+	 * @param minecraft The Minecraft instance
+	 * @return <code>true</code> if the camera should be coupled, <code>false</code> if the camera can be decoupled
+	 */
 	boolean isForcingCameraCoupling(Minecraft minecraft);
 }
--- api/src/main/java/com/github/exopandora/shouldersurfing/api/callback/ITargetCameraOffsetCallback.java
+++ api/src/main/java/com/github/exopandora/shouldersurfing/api/callback/ITargetCameraOffsetCallback.java
@@ -3,13 +3,30 @@ package com.github.exopandora.shouldersurfing.api.callback;
 import com.github.exopandora.shouldersurfing.api.client.IShoulderSurfing;
 import net.minecraft.world.phys.Vec3;
 
+/**
+ * This callback can be used to implement custom target camera offsets.
+ * There are two distinct methods, invoked at different time steps of the camera offset calculation.
+ * Both methods provide a default NOP implementation.
+ */
 public interface ITargetCameraOffsetCallback
 {
+	/**
+	 * @param instance The IShoulderSurfing instance
+	 * @param targetOffset The target offset for the camera
+	 * @param defaultOffset The default offset for the camera, without any prior modifications
+	 * @return The modified target offset for the camera
+	 */
 	default Vec3 pre(IShoulderSurfing instance, Vec3 targetOffset, Vec3 defaultOffset)
 	{
 		return targetOffset;
 	}
 	
+	/**
+	 * @param instance The IShoulderSurfing instance
+	 * @param targetOffset The target offset for the camera, after offset multipliers have been applied
+	 * @param defaultOffset The default offset for the camera, without any prior modifications
+	 * @return The modified target offset for the camera
+	 */
 	default Vec3 post(IShoulderSurfing instance, Vec3 targetOffset, Vec3 defaultOffset)
 	{
 		return targetOffset;
