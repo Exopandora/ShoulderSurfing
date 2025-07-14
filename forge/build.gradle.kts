c347bf9fbd041e142ee6cefa7989da1140d988db
Exopandora <mkonrad98@outlook.com>
2025-07-14 21:18:33 +0200

    Fix version for publishing

--- forge/build.gradle.kts
+++ forge/build.gradle.kts
@@ -97,7 +97,7 @@ tasks.named<ProcessResources>("processResources") {
 
 publishMods {
 	displayName = "$jarName-Forge-${libs.versions.minecraft.get()}-$modVersion"
-	version = "${rootProject.version}+forge"
+	version = "${project.version}+forge"
 	file = tasks.named<Jar>("jar").get().archiveFile
 	additionalFiles.from(
 		tasks.named("sourcesJar").get(),
