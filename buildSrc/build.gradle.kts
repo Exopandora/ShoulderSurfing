37093bf5c00dee20d3b6b7bfddb022ca0a62fe9c
Exopandora <mkonrad98@outlook.com>
2025-07-18 17:12:41 +0200

    Extract common publishing configuration

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,15 @@
 plugins {
     `kotlin-dsl`
+    alias(libs.plugins.modpublishplugin)
 }
 
 repositories {
     gradlePluginPortal()
 }
+
+dependencies {
+    implementation(plugin(libs.plugins.modpublishplugin))
+}
+
+fun DependencyHandlerScope.plugin(plugin: Provider<PluginDependency>) =
+    plugin.map { "${it.pluginId}:${it.pluginId}.gradle.plugin:${it.version}" }
