58544a89222d677abb2f34bcdbb6794620775232
Exopandora <mkonrad98@outlook.com>
2025-07-18 16:52:36 +0200

    Simplify source jar creation

--- forge/build.gradle.kts
+++ forge/build.gradle.kts
@@ -145,10 +145,6 @@ publishMods {
 	}
 }
 
-tasks.build {
-	finalizedBy("sourcesJar")
-}
-
 sourceSets.forEach {
 	val dir = layout.buildDirectory.dir("sourcesSets/${it.name}")
 	it.output.setResourcesDir(dir)
