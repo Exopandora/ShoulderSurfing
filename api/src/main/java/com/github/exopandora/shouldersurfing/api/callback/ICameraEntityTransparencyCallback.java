60b896394f72b50dd375ba9a2d7721417fa78f27
Exopandora <mkonrad98@outlook.com>
2025-12-04 20:12:13 +0100

    Add since annotations to API methods and classes

--- api/src/main/java/com/github/exopandora/shouldersurfing/api/callback/ICameraEntityTransparencyCallback.java
+++ api/src/main/java/com/github/exopandora/shouldersurfing/api/callback/ICameraEntityTransparencyCallback.java
@@ -6,6 +6,7 @@ import net.minecraft.world.entity.Entity;
 /**
  * This callback can be used to implement custom camera entity transparency rules.
  * The final result is the minimum value from all partial results.
+ * @since 4.14.0
  */
 public interface ICameraEntityTransparencyCallback
 {
@@ -14,6 +15,7 @@ public interface ICameraEntityTransparencyCallback
 	 * @param cameraEntity The camera entity
 	 * @param partialTick The render partial tick
 	 * @return The alpha value to use for camera entity rendering, ranging from <code>0.0F</code> (invisible) to <code>1.0F</code> (opaque)
+	 * @since 4.14.0
 	 */
 	float getCameraEntityAlpha(IShoulderSurfing instance, Entity cameraEntity, float partialTick);
 }
