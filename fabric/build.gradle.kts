56e7eb8d92e93897a5a2cb78eda6108a0743fa87
Exopandora <mkonrad98@outlook.com>
2025-12-13 19:33:29 +0100

    Update forge config api port slug for publishing

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -102,7 +102,7 @@ publishMods {
 	
 	curseforge {
 		minecraftVersions.set(compatibleVersions)
-		requires("fabric-api", "forge-config-api-port-fabric")
+		requires("fabric-api", "forge-config-api-port")
 		incompatible("better-third-person", "nimble-fabric", "valkyrien-skies")
 	}
 	
