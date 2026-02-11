6d294b41334257b062fa66aac55e49381cd8a208
Exopandora <mkonrad98@outlook.com>
2026-02-11 21:23:19 +0100

    Force camera coupling when riding a Minecart

--- common/src/main/java/com/github/exopandora/shouldersurfing/plugin/ShoulderSurfingPlugin.java
+++ common/src/main/java/com/github/exopandora/shouldersurfing/plugin/ShoulderSurfingPlugin.java
@@ -8,6 +8,7 @@ import com.github.exopandora.shouldersurfing.compat.plugin.CobblemonAdaptiveItem
 import com.github.exopandora.shouldersurfing.compat.plugin.CreateModTargetCameraOffsetCallback;
 import com.github.exopandora.shouldersurfing.compat.plugin.ICuriosAdaptiveItemCallback;
 import com.github.exopandora.shouldersurfing.plugin.callbacks.AdaptiveItemCallback;
+import com.github.exopandora.shouldersurfing.plugin.callbacks.CameraCouplingCallback;
 import com.github.exopandora.shouldersurfing.plugin.callbacks.CameraEntityTransparencyCallback;
 import com.github.exopandora.shouldersurfing.plugin.callbacks.CameraEntityTransparencyCallbackWhenAiming;
 import org.apache.commons.lang3.StringUtils;
@@ -20,6 +21,7 @@ public class ShoulderSurfingPlugin implements IShoulderSurfingPlugin
 	public void register(IShoulderSurfingRegistrar registrar)
 	{
 		registrar.registerAdaptiveItemCallback(new AdaptiveItemCallback());
+		registrar.registerCameraCouplingCallback(new CameraCouplingCallback());
 		registrar.registerCameraEntityTransparencyCallback(new CameraEntityTransparencyCallback());
 		registrar.registerCameraEntityTransparencyCallback(new CameraEntityTransparencyCallbackWhenAiming());
 		registerCompatibilityCallback(Mods.CREATE, () -> registrar.registerTargetCameraOffsetCallback(new CreateModTargetCameraOffsetCallback()));
