b2f65c4e74b8eb4ed41a7802f93ccc0b6e671428
Exopandora <mkonrad98@outlook.com>
2026-04-26 16:54:01 +0200

    Extract custom render types to ShoulderSurfingRenderTypes

---
+++ common/src/main/java/com/github/exopandora/shouldersurfing/client/ShoulderSurfingRenderTypes.java
@@ -0,0 +1,52 @@
+package com.github.exopandora.shouldersurfing.client;
+
+import com.github.exopandora.shouldersurfing.mixins.RenderTypeAccessor;
+import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.client.renderer.feature.ItemFeatureRenderer;
+import net.minecraft.client.renderer.rendertype.LayeringTransform;
+import net.minecraft.client.renderer.rendertype.OutputTarget;
+import net.minecraft.client.renderer.rendertype.RenderSetup;
+import net.minecraft.client.renderer.rendertype.RenderType;
+import net.minecraft.client.renderer.rendertype.TextureTransform;
+import net.minecraft.resources.Identifier;
+import net.minecraft.util.Util;
+
+import java.util.function.Function;
+
+public class ShoulderSurfingRenderTypes
+{
+	private static final RenderType ARMOR_ENTITY_GLINT_ITEM_TARGET = RenderTypeAccessor.invokeCreate(
+		"armor_entity_glint_item_target",
+		RenderSetup.builder(RenderPipelines.GLINT)
+			.withTexture("Sampler0", ItemFeatureRenderer.ENCHANTED_GLINT_ARMOR)
+			.setTextureTransform(TextureTransform.ARMOR_ENTITY_GLINT_TEXTURING)
+			.setLayeringTransform(LayeringTransform.VIEW_OFFSET_Z_LAYERING)
+			.setOutputTarget(OutputTarget.ITEM_ENTITY_TARGET)
+			.createRenderSetup()
+	);
+	
+	private static final Function<Identifier, RenderType> ARMOR_TRANSLUCENT_ITEM_TARGET = Util.memoize(texture ->
+	{
+		RenderSetup state = RenderSetup.builder(RenderPipelines.ARMOR_TRANSLUCENT)
+			.withTexture("Sampler0", texture)
+			.useLightmap()
+			.useOverlay()
+			.setLayeringTransform(LayeringTransform.VIEW_OFFSET_Z_LAYERING)
+			.affectsCrumbling()
+			.sortOnUpload()
+			.setOutline(RenderSetup.OutlineProperty.AFFECTS_OUTLINE)
+			.setOutputTarget(OutputTarget.ITEM_ENTITY_TARGET)
+			.createRenderSetup();
+		return RenderTypeAccessor.invokeCreate("armor_translucent_item_target", state);
+	});
+	
+	public static RenderType armorEntityGlintItemTarget()
+	{
+		return ARMOR_ENTITY_GLINT_ITEM_TARGET;
+	}
+	
+	public static RenderType armorTranslucentItemTarget(Identifier texture)
+	{
+		return ARMOR_TRANSLUCENT_ITEM_TARGET.apply(texture);
+	}
+}
