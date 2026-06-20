ea27c64c6da4ad013053f79028a0dfb706d07892
Exopandora <mkonrad98@outlook.com>
2026-06-20 17:47:44 +0200

    Add option to control player transparency effects when climbing (disabled by default). Closes #437

--- common/src/main/java/com/github/exopandora/shouldersurfing/plugin/BuiltinPlugin.java
+++ common/src/main/java/com/github/exopandora/shouldersurfing/plugin/BuiltinPlugin.java
@@ -30,9 +30,11 @@ public class BuiltinPlugin implements IShoulderSurfingPlugin {
 		eventBus.register(ComputePlayerAimStateEventHandlerImpl.INSTANCE);
 		eventBus.register(2000, SetupCameraRotationEventHandlerImpl.INSTANCE);
 		eventBus.register(ComputeCameraCouplingEventHandlerImpl.INSTANCE);
-		eventBus.register(ComputeCameraEntityTransparencyEventHandlerImpl.INSTANCE);
-		eventBus.register((TickEventHandler) ComputeCameraEntityTransparencyEventHandlerImpl.WhenAiming.INSTANCE);
-		eventBus.register((ComputeCameraEntityTransparencyEventHandler) ComputeCameraEntityTransparencyEventHandlerImpl.WhenAiming.INSTANCE);
+		eventBus.register(1000, ComputeCameraEntityTransparencyEventHandlerImpl.INSTANCE);
+		eventBus.register(1500, (TickEventHandler) ComputeCameraEntityTransparencyEventHandlerImpl.Climbing.INSTANCE);
+		eventBus.register(1500, (ComputeCameraEntityTransparencyEventHandler) ComputeCameraEntityTransparencyEventHandlerImpl.Climbing.INSTANCE);
+		eventBus.register(2000, (TickEventHandler) ComputeCameraEntityTransparencyEventHandlerImpl.WhenAiming.INSTANCE);
+		eventBus.register(2000, (ComputeCameraEntityTransparencyEventHandler) ComputeCameraEntityTransparencyEventHandlerImpl.WhenAiming.INSTANCE);
 		eventBus.register(0, ComputePlayerAttackStateEventHandlerImpl.Pre.INSTANCE);
 		eventBus.register(2000, ComputePlayerAttackStateEventHandlerImpl.Post.INSTANCE);
 		eventBus.register(0, ComputePlayerInteractionStateEventHandlerImpl.Pre.INSTANCE);
