3313086752f4779b9f11eed62a67e03237864e49
Exopandora <mkonrad98@outlook.com>
2026-06-21 21:46:09 +0200

    Convert changelog to markdown

--- buildSrc/src/main/kotlin/multiloader-modloader.gradle.kts
+++ buildSrc/src/main/kotlin/multiloader-modloader.gradle.kts
@@ -48,7 +48,7 @@ publishMods {
         tasks.named("apiJar").get(),
         tasks.named("apiSourcesJar").get()
     )
-    changelog = provider { file("../changelog.txt").readText() }
+    changelog = provider { rootProject.file("CHANGELOG.md").readText() }
     type = STABLE
     
     curseforge {
