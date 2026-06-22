9ad8b95f98af79fd920b426e784fda00468add4b
Exopandora <mkonrad98@outlook.com>
2026-06-22 23:24:32 +0200

    Improve compatibility with sable. Closes #423

--- common/src/main/java/com/github/exopandora/shouldersurfing/mixin/CameraMixin.java
+++ common/src/main/java/com/github/exopandora/shouldersurfing/mixin/CameraMixin.java
@@ -10,6 +10,9 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -22,6 +25,24 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Camera.class)
 public abstract class CameraMixin implements CameraDuck {
 	@Shadow
+	private static @Final Vector3f FORWARDS;
+	
+	@Shadow
+	private static @Final Vector3f UP;
+	
+	@Shadow
+	private static @Final Vector3f LEFT;
+	
+	@Shadow
+	private @Final Vector3f forwards;
+	
+	@Shadow
+	private @Final Vector3f up;
+	
+	@Shadow
+	private @Final Vector3f left;
+	
+	@Shadow
 	private float xRot;
 	
 	@Shadow
@@ -29,6 +50,9 @@ public abstract class CameraMixin implements CameraDuck {
 	
 	@Unique
 	private float zRot;
+	
+	@Shadow
+	private @Final Quaternionf rotation;
 	
 	@Shadow
 	protected abstract void move(float x, float y, float z);
@@ -85,11 +109,25 @@ public abstract class CameraMixin implements CameraDuck {
 			Vec3 cameraOffset = camera.getRenderOffset();
 			this.move((float) -cameraOffset.z(), (float) cameraOffset.y(), (float) -cameraOffset.x());
 			Vec2f sway = camera.calcSway(cameraEntity, partialTick);
-			this.zRot = sway.y();
-			this.setRotation(this.yRot, this.xRot + sway.x());
+			this.shouldersurfing$rotate(sway.x(), 0, sway.y());
 		} else {
 			this.move(x, y, z);
 		}
+	}
+	
+	@Unique
+	private void shouldersurfing$rotate(float xRot, float yRot, float zRot) {
+		this.xRot += xRot;
+		this.yRot += yRot;
+		this.zRot += zRot;
+		this.rotation.rotationYXZ(
+			(float) Math.PI - this.yRot * (float) (Math.PI / 180.0),
+			-this.xRot * (float) (Math.PI / 180.0),
+			-this.zRot * (float) (Math.PI / 180.0)
+		);
+		FORWARDS.rotate(this.rotation, this.forwards);
+		UP.rotate(this.rotation, this.up);
+		LEFT.rotate(this.rotation, this.left);
 	}
 	
 	@Override
