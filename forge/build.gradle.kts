c63552c13db5bf90b579c4718a33aa829aff90a2
Exopandora <mkonrad98@outlook.com>
2026-06-30 18:26:14 +0200

    Remove valkyrien skies incompatibility declaration

--- forge/build.gradle.kts
+++ forge/build.gradle.kts
@@ -106,12 +106,12 @@ publishMods {
 	
 	curseforge {
 		minecraftVersions.set(compatibleVersions)
-		incompatible("better-third-person", "nimble", "valkyrien-skies", "ydms-custom-camera-view")
+		incompatible("better-third-person", "nimble", "ydms-custom-camera-view")
 	}
 	
 	modrinth {
 		minecraftVersions.set(compatibleVersions)
-		incompatible("better-third-person", "nimble", "valkyrien-skies", "ydms-custom-camera-view")
+		incompatible("better-third-person", "nimble", "ydms-custom-camera-view")
 	}
 }
 
