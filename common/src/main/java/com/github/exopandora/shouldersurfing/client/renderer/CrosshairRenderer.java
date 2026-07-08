d2657ad5f2f7b38da9183cb63149de0920f02401
Exopandora <mkonrad98@outlook.com>
2026-07-08 10:52:03 +0200

    Fix obstruction indicator remaining visible after switching perspective. Closes #445

--- common/src/main/java/com/github/exopandora/shouldersurfing/client/renderer/CrosshairRenderer.java
+++ common/src/main/java/com/github/exopandora/shouldersurfing/client/renderer/CrosshairRenderer.java
@@ -71,6 +71,9 @@ public class CrosshairRenderer implements ICrosshairRenderer {
 			this.isObstructionCrosshairVisible = computeIsObstructionCrosshairVisible(
 				this.instance.isAiming(), this.isObstructionIndicatorVisible
 			);
+		} else {
+			this.isObstructionIndicatorVisible = false;
+			this.isObstructionCrosshairVisible = false;
 		}
 	}
 	
