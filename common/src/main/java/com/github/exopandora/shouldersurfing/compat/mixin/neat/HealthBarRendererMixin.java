2cc3b2d995187c0c53c4a0803f9c833c169dd7ba
Exopandora <mkonrad98@outlook.com>
2026-07-13 17:38:12 +0200

    Align compat mixin class, method and field visibility modifiers

--- common/src/main/java/com/github/exopandora/shouldersurfing/compat/mixin/neat/HealthBarRendererMixin.java
+++ common/src/main/java/com/github/exopandora/shouldersurfing/compat/mixin/neat/HealthBarRendererMixin.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Pseudo
 @Mixin(targets = "vazkii.neat.HealthBarRenderer")
-public class HealthBarRendererMixin {
+class HealthBarRendererMixin {
 	@Inject(
 		method = "getEntityLookedAt",
 		at = @At("HEAD"),
--- common/src/main/java/com/github/exopandora/shouldersurfing/compat/mixin/theoneprobe/OverlayRendererMixin.java
+++ common/src/main/java/com/github/exopandora/shouldersurfing/compat/mixin/theoneprobe/OverlayRendererMixin.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Pseudo
 @Mixin(targets = "mcjty.theoneprobe.rendering.OverlayRenderer")
-public class OverlayRendererMixin {
+class OverlayRendererMixin {
 	@Redirect(
 		method = "renderHUD",
 		at = @At(
