1f449b6b5c5a018ef82858a56b6332a403281ad5
Exopandora <mkonrad98@outlook.com>
2026-07-13 11:51:52 +0200

    Align assertion error messages

--- compat/src/create-0.5.0/java/com/simibubi/create/foundation/utility/Couple.java
+++ compat/src/create-0.5.0/java/com/simibubi/create/foundation/utility/Couple.java
@@ -2,6 +2,6 @@ package com.simibubi.create.foundation.utility;
 
 public class Couple<T> {
 	public static <T> Couple<T> create(T first, T second) {
-		throw new AssertionError("Not implemented");
+		throw new AssertionError();
 	}
 }
