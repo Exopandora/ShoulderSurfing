7a88e8cc660382bfb70a60bfd0a7325290c5e030
Exopandora <mkonrad98@outlook.com>
2026-07-23 18:26:33 +0200

    Add back missing shader mixin compatibility code for neoforge (closes #453)

--- neoforge/src/main/java/com/github/exopandora/shouldersurfing/neoforge/compat/ShoulderSurfingCompatMixinPluginNeoForge.java
+++ neoforge/src/main/java/com/github/exopandora/shouldersurfing/neoforge/compat/ShoulderSurfingCompatMixinPluginNeoForge.java
@@ -2,12 +2,15 @@ package com.github.exopandora.shouldersurfing.neoforge.compat;
 
 import com.github.exopandora.shouldersurfing.compat.Mods;
 import com.github.exopandora.shouldersurfing.compat.ShoulderSurfingCompatMixinPlugin;
+import org.apache.maven.artifact.versioning.ArtifactVersion;
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
+import org.jetbrains.annotations.Nullable;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import static com.github.exopandora.shouldersurfing.neoforge.Platform.parseVersionRangeSilent;
 
@@ -18,7 +21,19 @@ public class ShoulderSurfingCompatMixinPluginNeoForge extends ShoulderSurfingCom
 		addCobblemonMixins(mixins);
 		addCommonCompatMixins(mixins);
 		addCreateModMixins(mixins);
+		addShaderMixins(mixins);
 		return mixins.isEmpty() ? null : mixins;
+	}
+	
+	private static void addShaderMixins(List<String> mixins) {
+		var version = highestShaderVersion();
+		if (version != null) {
+			if (parseVersionRangeSilent("[1.7.0-snapshot,)").containsVersion(version)) {
+				mixins.add("iris.MixinSheets_1_7_0");
+			} else if (parseVersionRangeSilent("[1.6.15,1.7.0)").containsVersion(version)) {
+				mixins.add("iris.MixinSheets_1_6_15");
+			}
+		}
 	}
 	
 	private static void addCreateModMixins(List<String> mixins) {
@@ -55,5 +70,21 @@ public class ShoulderSurfingCompatMixinPluginNeoForge extends ShoulderSurfingCom
 	@Override
 	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
 		
+	}
+	
+	private static @Nullable ArtifactVersion highestShaderVersion() {
+		List<String> shaderVersions = new ArrayList<String>();
+		shaderVersions.add(Mods.OCULUS.getModVersion());
+		shaderVersions.add(Mods.IRIS.getModVersion());
+		shaderVersions.removeIf(Objects::isNull);
+		return switch (shaderVersions.size()) {
+			case 0 -> null;
+			case 1 -> new DefaultArtifactVersion(shaderVersions.getFirst());
+			default -> shaderVersions.stream()
+				.map(DefaultArtifactVersion::new)
+				.sorted()
+				.findFirst()
+				.orElse(null);
+		};
 	}
 }
