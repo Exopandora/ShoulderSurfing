2de1a08352658ce672933684b709295b4e7f69bd
Exopandora <mkonrad98@outlook.com>
2026-08-30 14:23:21 +0200

    Fix camera rotations when flying cobblemon rides

--- common/src/main/java/com/github/exopandora/shouldersurfing/compat/cobblemon/event/handler/CobblemonEventHandler.java
+++ common/src/main/java/com/github/exopandora/shouldersurfing/compat/cobblemon/event/handler/CobblemonEventHandler.java
@@ -1,8 +1,14 @@
 package com.github.exopandora.shouldersurfing.compat.cobblemon.event.handler;
 
+//import com.cobblemon.mod.common.OrientationControllable;
+//import com.cobblemon.mod.common.api.riding.behaviour.types.liquid.BoatBehaviour;
+//import com.cobblemon.mod.common.api.riding.behaviour.types.liquid.DolphinBehaviour;
+//import com.cobblemon.mod.common.api.riding.behaviour.types.liquid.SubmarineBehaviour;
+//import com.cobblemon.mod.common.entity.pokemon.PokemonEntity;
 import com.cobblemon.mod.common.OrientationControllable;
 import com.cobblemon.mod.common.item.PokeBallItem;
 import com.cobblemon.mod.common.item.interactive.PokerodItem;
+import com.github.exopandora.shouldersurfing.api.client.event.ComputeCameraSwayEvent;
 import com.github.exopandora.shouldersurfing.api.client.event.ComputePlayerAimStateEvent;
 import com.github.exopandora.shouldersurfing.api.client.event.ComputePlayerRideBoatStateEvent;
 import com.github.exopandora.shouldersurfing.api.client.event.ForceVanillaPlayerInputEvent;
@@ -10,16 +16,11 @@ import com.github.exopandora.shouldersurfing.api.client.event.SetupCameraRotatio
 import com.github.exopandora.shouldersurfing.api.math.Vec2f;
 import com.github.exopandora.shouldersurfing.compat.Mods;
 import net.minecraft.client.Minecraft;
+//import net.minecraft.resources.Identifier;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
-
-//import com.cobblemon.mod.common.api.riding.behaviour.ActiveRidingContext;
-//import com.cobblemon.mod.common.api.riding.behaviour.types.liquid.BoatBehaviour;
-//import com.cobblemon.mod.common.api.riding.behaviour.types.liquid.DolphinBehaviour;
-//import com.cobblemon.mod.common.api.riding.behaviour.types.liquid.SubmarineBehaviour;
-//import com.cobblemon.mod.common.entity.pokemon.PokemonEntity;
 
 public enum CobblemonEventHandler {
 	INSTANCE;
@@ -70,10 +71,17 @@ public enum CobblemonEventHandler {
 			var vehicle = event.getPlayer().getVehicle();
 			var partialTick = Minecraft.getInstance().getDeltaTracker().getGameTimeDeltaPartialTick(true);
 			var yRotLerped = Mth.rotLerp(partialTick, vehicle.yRotO, vehicle.getYRot());
-			var delta = Mth.wrapDegrees(event.getResult().y()- yRotLerped);
+			var delta = Mth.wrapDegrees(event.getResult().y() - yRotLerped);
 			var clamped = Mth.clamp(delta, -105.0F, 105.0F);
 			event.setResult(event.getResult().add(0, clamped - delta));
 		}
+	}
+	
+	public void computeCameraSway(ComputeCameraSwayEvent event) {
+//		var cameraEntity = Minecraft.getInstance().getCameraEntity();
+//		if (cameraEntity != null && cameraEntity.getVehicle() instanceof OrientationControllable) {
+//			event.cancel();
+//		}
 	}
 	
 	private static boolean hasActiveBoatBehaviour(@Nullable Entity vehicle) {
